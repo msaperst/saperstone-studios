fe97fe26147d4067182493f9a3d5e54ebfd713c3
Max Saperstone <max.saperstone@coveros.com>
2016-05-20 18:27:31 -0400

    Adding locationbox to the homepage

--- index.php
+++ index.php
@@ -3,39 +3,13 @@
 
 <head>
 
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description"
-	content="Photography and Retouch for Virginia, DC, and Maryland by Saperstone Studios">
-<meta name="author" content="Saperstone Studios">
-
-<title>Photography and Retouch for Virginia, DC, and Maryland by
-	Saperstone Studios</title>
-
-<!-- Bootstrap Core CSS -->
-<link href="css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-<link href="css/modern-business.css" rel="stylesheet">
-<link href="css/saperstone-studios.css" rel="stylesheet">
-
-<!-- Custom Fonts -->
-<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"
-	type="text/css">
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+	<?php require "header.php"; ?>
 
 </head>
 
 <body>
 
-    <?php require "nav.php"; ?>
+    <?php require "nav-main.php"; ?>
 
     <!-- Page Content -->
 	<div class="page-content container">
@@ -72,11 +46,12 @@
 				<h2 class="page-header">Welcome to Saperstone Studios</h2>
 			</div>
 			<div class="col-md-12">
+				<img src='img/locationbox.png' align='right' />
 				<p>Allow me to introduce myself! I've worked in the photography
 					industry for over 10 years, coordinating photo shoots and
 					performing high level color management and retouch for program
-					books and billboards for Ringling Bros. Barnum & Bailey Circus [[So
-					many elephants!]] as well as a local DC fashion company managing
+					books and billboards for Ringling Bros. Barnum & Bailey Circus <i>[[So
+					many elephants!]]</i> as well as a local DC fashion company managing
 					the retouch workflow and consistent quality control.</p>
 				<p class="lead">I've started Saperstone Studios with the hope of
 					providing not just photography, but also an experience.</p>
@@ -110,25 +85,10 @@
 		</div>
 		<!-- /.row -->
 
-		<hr>
-        
         <?php require "footer.php"; ?>
 
     </div>
 	<!-- /.container -->
-
-	<!-- jQuery -->
-	<script src="js/jquery.js"></script>
-
-	<!-- Bootstrap Core JavaScript -->
-	<script src="js/bootstrap.min.js"></script>
-
-	<!-- Script to Activate the Carousel -->
-	<script>
-    $('.carousel').carousel({
-        interval: 5000 //changes the speed
-    })
-    </script>
 
 </body>
 
