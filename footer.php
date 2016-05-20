7068536c76b00dde7a36fb6c2cbdbb64d4c86e2e
Max Saperstone <max.saperstone@coveros.com>
2016-05-19 23:29:46 -0400

    Initial updates to match style to SS, and design for homepage

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!-- Footer -->
+<footer>
+	<div class="row">
+		<div class="col-md-4 text-left">
+			<ul class="list-inline quicklinks">
+				<li><a target="_blank" href="mailto:contact@saperstonestudios.com">Contact@SaperstoneStudios.com</a>
+				</li>
+				<li><a target="_blank" href="tel:5712660004">571.266.0004</a></li>
+			</ul>
+		</div>
+		<div class="col-md-4 text-center">
+			<ul class="list-inline social-buttons">
+				<li><a target="_blank" href="https://www.facebook.com/SaperstoneStudios"><i
+						class="fa fa-facebook"></i></a></li>
+				<li><a target="_blank" href="http://instagram.com/lasaperstone"><i
+						class="fa fa-instagram"></i></a></li>
+				<li><a target="_blank" href="https://twitter.com/LaSaperstone"><i
+						class="fa fa-twitter"></i></a></li>
+				<li><a target="_blank" href="https://saperstonestudios.com/Blog/blogFeed.xml"><i
+						class="fa fa-rss"></i></a></li>
+				<li><a target="_blank" href="https://plus.google.com/+SaperstoneStudios"><i
+						class="fa fa-google-plus"></i></a></li>
+			</ul>
+		</div>
+		<div class="col-md-4 text-right">
+			<ul class="list-inline quicklinks">
+				<li><a href="/Privacy-Policy.php">Privacy Policy</a>
+				</li>
+				<li><a href="/Terms-of-Use.php">Terms of Use</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<span class="copyright">Copyright &copy Saperstone Studios <?php echo date("Y"); ?></span>
+		</div>
+	</div>
+</footer>
