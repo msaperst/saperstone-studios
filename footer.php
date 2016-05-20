a5b181812fa33d2654f36c23d4339a17eaf13ac3
Max Saperstone <max.saperstone@coveros.com>
2016-05-20 18:27:59 -0400

    Small configuration tweaks for better viewablility of site

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<hr>
+
 <!-- Footer -->
 <footer>
 	<div class="row">
@@ -10,22 +12,24 @@
 		</div>
 		<div class="col-md-4 text-center">
 			<ul class="list-inline social-buttons">
-				<li><a target="_blank" href="https://www.facebook.com/SaperstoneStudios"><i
+				<li><a target="_blank"
+					href="https://www.facebook.com/SaperstoneStudios"><i
 						class="fa fa-facebook"></i></a></li>
 				<li><a target="_blank" href="http://instagram.com/lasaperstone"><i
 						class="fa fa-instagram"></i></a></li>
 				<li><a target="_blank" href="https://twitter.com/LaSaperstone"><i
 						class="fa fa-twitter"></i></a></li>
-				<li><a target="_blank" href="https://saperstonestudios.com/Blog/blogFeed.xml"><i
+				<li><a target="_blank"
+					href="https://saperstonestudios.com/Blog/blogFeed.xml"><i
 						class="fa fa-rss"></i></a></li>
-				<li><a target="_blank" href="https://plus.google.com/+SaperstoneStudios"><i
+				<li><a target="_blank"
+					href="https://plus.google.com/+SaperstoneStudios"><i
 						class="fa fa-google-plus"></i></a></li>
 			</ul>
 		</div>
 		<div class="col-md-4 text-right">
 			<ul class="list-inline quicklinks">
-				<li><a href="/Privacy-Policy.php">Privacy Policy</a>
-				</li>
+				<li><a href="/Privacy-Policy.php">Privacy Policy</a></li>
 				<li><a href="/Terms-of-Use.php">Terms of Use</a></li>
 			</ul>
 		</div>
@@ -36,3 +40,19 @@
 		</div>
 	</div>
 </footer>
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/nav.js"></script>
+
+<!-- Script to Activate the Carousel -->
+<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+session_name('ssLogin');
+// Starting the session
+
+session_set_cookie_params(2*7*24*60*60);
+// Making the cookie live for 2 weeks
+
+session_start();
+?>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description"
+	content="Photography and Retouch for Virginia, DC, and Maryland by Saperstone Studios">
+<meta name="author" content="Saperstone Studios">
+
+<title>Photography and Retouch for Virginia, DC, and Maryland by
+	Saperstone Studios</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="css/modern-business.css" rel="stylesheet">
+<link href="css/saperstone-studios.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"
+	type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+<?php require "php/user.php"; ?>
