97c179eb0a9b9f4a8eae157b1411e30fb88926ff
Max Saperstone <max.saperstone@coveros.com>
2016-05-20 18:25:53 -0400

    Updated the contact page to SS details

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -16,10 +16,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'your-email@your-domain.com'; // PUT YOUR EMAIL ADDRESS HERE
-$email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$to = 'la@saperstonestudios.com'; // PUT YOUR EMAIL ADDRESS HERE
+$email_subject = "Saperstone Studios Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@your-domain.com\n";
+$headers = "From: noreply@saperstonestudios.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
