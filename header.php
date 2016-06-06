01f25327fcaddd370c33e182d5e14a64eec48d13
Max Saperstone <max.saperstone@coveros.com>
2016-06-06 17:37:33 -0400

    Added integrity checks to CDN resources

--- header.php
+++ header.php
@@ -28,7 +28,9 @@ $nav = "main";
 <!-- Bootstrap Core CSS -->
 <link
 	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-	rel="stylesheet">
+	rel="stylesheet"
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	crossorigin="anonymous">
 
 <!-- Custom CSS -->
 <link href="/css/modern-business.css" rel="stylesheet">
