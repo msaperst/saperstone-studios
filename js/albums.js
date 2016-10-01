75ed490e61b631467dc91ff918bb06011ba030ac
Max Saperstone <msaperst@gmail.com>
2016-09-30 20:46:22 -0400

    Added functionality for user to add an album to their album list via code

--- js/albums.js
+++ js/albums.js
@@ -39,6 +39,32 @@ $(document).ready(function() {
 });
 
 function addAlbum() {
-    //TODO
-    BootstrapDialog.alert("Sorry, this functionality doesn't yet exist");
+	// spin our button
+	$("#album-code-add").prop("disabled", true);
+    $("#album-code-add em").removeClass('fa fa-plus-circle').addClass('glyphicon glyphicon-asterisk icon-spin');
+	// make our call
+    $.get("/api/find-album.php", {
+        code : $('#album-code').val(),
+        albumAdd : 1,
+    }).done(function(data) {
+        // goto album url if it exists
+        if ($.isNumeric(data) && data !== '0') {
+        	$('#add-album-div').append("<div id='album-code-add-message' class='alert alert-info'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Added album to your list</div>");                    	
+        	// refresh our tables
+        	album_table.ajax.reload(null, false);
+        	setTimeout(function(){
+        		$('#album-code-add-message').remove();
+        	}, 10000);
+        } else if (data === '0') {
+        	$('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Some unexpected error occurred while searching for your album.<br/>Please <a class='gen' target='_blank' href='mailto:admin@saperstonestudios.com'>Contact our System Administrators</a> for more details, or try resubmitting.</div>");
+        } else {
+        	$('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>" + data + "</div>");
+        }
+    }).fail(function(){
+    	$('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Some unexpected error occurred while searching for your album.<br/>Please <a class='gen' target='_blank' href='mailto:admin@saperstonestudios.com'>Contact our System Administrators</a> for more details, or try resubmitting.</div>");
+    }).always(function(){
+    	//fix our button
+    	$("#album-code-add").prop("disabled", false);
+        $("#album-code-add em").addClass('fa fa-plus-circle').removeClass('glyphicon glyphicon-asterisk icon-spin');
+    });
 }
