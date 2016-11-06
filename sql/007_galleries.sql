c0e6e0b0dd9e5833fe1d5f006d796b34863d4a44
Max Saperstone <msaperst@gmail.com>
2016-11-06 09:30:22 -0500

    Fixing display of 'sub-cateories' in galleries

--- sql/007_galleries.sql
+++ sql/007_galleries.sql
@@ -6,6 +6,8 @@
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE IF NOT EXISTS `galleries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dir` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dir_link` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
