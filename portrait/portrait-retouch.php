201436527655854283ab32d21f84d5a52ddbad6c
Max Saperstone <msaperst@gmail.com>
2017-01-29 09:33:04 -0500

    Fixing spacing; updating todo list

--- portrait/portrait-retouch.php
+++ portrait/portrait-retouch.php
@@ -18,52 +18,52 @@
     ?>
 
     <!-- Page Content -->
-	<div class="page-content container">
+    <div class="page-content container">
 
-		<!-- Page Heading/Breadcrumbs -->
-		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="page-header text-center">Retouch</h1>
-				<ol class="breadcrumb">
-					<li><a href="/">Home</a></li>
-					<li><a href="index.php">Portraits</a></li>
-					<li><a href="retouch.php">Retouch</a></li>
-					<li class="active">Retouch</li>
-				</ol>
-			</div>
-		</div>
-		<!-- /.row -->
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header text-center">Retouch</h1>
+                <ol class="breadcrumb">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="index.php">Portraits</a></li>
+                    <li><a href="retouch.php">Retouch</a></li>
+                    <li class="active">Retouch</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
 
-		<!-- Portraits Retouch -->
-		<div class="row" style="margin-top: 30px;">
-			<div class="col-lg-12">
-				<p>Below are some examples of when a little retouch TLC goes a long
-					way when it comes to making your images perfect. Most of the time,
-					you won't even realize this behind the scenes magic has even
-					happened by the time you see your images. If you would like any
-					additional retouch after seeing your images I'm happy to
-					accommodate if the requests are minimal/standard. Otherwise a small
-					fee may be negotiated.</p>
-				<p>Click the thumbnails below and use the slider at the bottom of
-					the image to see the before/after transformation.</p>
-			</div>
-		</div>
-		<div class="row" style="margin-top: 30px;">
-			<!-- Content Column -->
-			<div class="col-md-offset-2 col-md-8">
-				<div class='text-center'>
-					<div id='holder' class='holder'></div>
-				</div>
-			</div>
-		</div>
+        <!-- Portraits Retouch -->
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-lg-12">
+                <p>Below are some examples of when a little retouch TLC goes a long
+                    way when it comes to making your images perfect. Most of the time,
+                    you won't even realize this behind the scenes magic has even
+                    happened by the time you see your images. If you would like any
+                    additional retouch after seeing your images I'm happy to
+                    accommodate if the requests are minimal/standard. Otherwise a small
+                    fee may be negotiated.</p>
+                <p>Click the thumbnails below and use the slider at the bottom of
+                    the image to see the before/after transformation.</p>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 30px;">
+            <!-- Content Column -->
+            <div class="col-md-offset-2 col-md-8">
+                <div class='text-center'>
+                    <div id='holder' class='holder'></div>
+                </div>
+            </div>
+        </div>
 
         <?php require_once "../footer.php"; ?>
 
     </div>
-	<!-- /.container -->
+    <!-- /.container -->
 
-	<script src='/js/retouch.js'></script>
-	<script>
+    <script src='/js/retouch.js'></script>
+    <script>
         var images = [];
         images[0] = { thumb:'/retouch/img/Avon_20141127_0092.jpg', orig:'/retouch/img/Avon_20141127_0092_o.jpg', edit:'/retouch/img/Avon_20141127_0092_e.jpg', width:'1140', height:'759', text:'When your adorable 2 year old isn\'t trusted with your 1 week old quite yet.' };
         images[1] = { thumb:'/retouch/img/Eliza_20160729_0008.jpg', orig:'/retouch/img/Eliza_20160729_0008_o.jpg', edit:'/retouch/img/Eliza_20160729_0008_e.jpg', width:'1140', height:'761', text:'Newborns are heavy!  Getting the right angle for both of these cuties required a bit of post retouch.' };
--- wedding/engagement.php
+++ wedding/engagement.php
@@ -14,11 +14,11 @@
         $rand = "?" . $string->randomString ();
         ?>
     <link
-	href="http://hayageek.github.io/jQuery-Upload-File/4.0.10/uploadfile.css"
-	rel="stylesheet">
+    href="http://hayageek.github.io/jQuery-Upload-File/4.0.10/uploadfile.css"
+    rel="stylesheet">
 <link
-	href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.css"
-	rel="stylesheet">
+    href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.css"
+    rel="stylesheet">
     <?php
     }
     ?>
@@ -30,75 +30,75 @@
     <?php $nav = "wedding"; require_once "../nav.php"; ?>
 
     <!-- Page Content -->
-	<div class="page-content container">
+    <div class="page-content container">
 
-		<!-- Page Heading/Breadcrumbs -->
-		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="page-header text-center">Engagement Session</h1>
-				<ol class="breadcrumb">
-					<li><a href="/">Home</a></li>
-					<li><a href="index.php">Weddings</a></li>
-					<li><a href="details.php">Details</a></li>
-					<li class="active">Engagements</li>
-				</ol>
-			</div>
-		</div>
-		<!-- /.row -->
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header text-center">Engagement Session</h1>
+                <ol class="breadcrumb">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="index.php">Weddings</a></li>
+                    <li><a href="details.php">Details</a></li>
+                    <li class="active">Engagements</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
 
-		<!-- Features Section -->
-		<div class="row">
-			<div class="col-xs-6">
-				<div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-					<img width="100%" src="img/engagement-main-1.jpg<?php echo $rand; ?>"
-						alt="Engagements">
-				</div>
-			</div>
-			<div class="col-xs-6">
-				<div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-					<img width="100%" src="img/engagement-main-2.jpg<?php echo $rand; ?>"
-						alt="Engagements">
-				</div>
-			</div>
-		</div>
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+                    <img width="100%" src="img/engagement-main-1.jpg<?php echo $rand; ?>"
+                        alt="Engagements">
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+                    <img width="100%" src="img/engagement-main-2.jpg<?php echo $rand; ?>"
+                        alt="Engagements">
+                </div>
+            </div>
+        </div>
 
-		<div class="row" style='padding-top: 30px'>
-			<div class="col-lg-12">
-			<h2>Congrats on your engagement!</h2>
-				<p>
-					I love, love, love engagement sessions
-					and getting to know my clients without the rush and hurry on your
-					big day. That's why I offer a complimentary <a
-						href='galleries.php?w=20'>engagement session</a> in all of my
-					wedding packages. It offers the opportunity for you to be
-					comfortable in front of the lens and to see how I work. I get the
-					chance to see you two as a couple and what makes you smile, laugh
-					and get some wonderful natural reactions that are candid and you.
-				</p>
-				<p>
-					Interested in just the engagement session without the <a
-						href='galleries.php?w=24'>wedding photography</a>? That's alright
-					too! <a href='/contact.php'>Contact me</a> today for more
-					information.
-				</p>
-			</div>
-		</div>
-		<!-- /.row -->
+        <div class="row" style='padding-top: 30px'>
+            <div class="col-lg-12">
+            <h2>Congrats on your engagement!</h2>
+                <p>
+                    I love, love, love engagement sessions
+                    and getting to know my clients without the rush and hurry on your
+                    big day. That's why I offer a complimentary <a
+                        href='galleries.php?w=20'>engagement session</a> in all of my
+                    wedding packages. It offers the opportunity for you to be
+                    comfortable in front of the lens and to see how I work. I get the
+                    chance to see you two as a couple and what makes you smile, laugh
+                    and get some wonderful natural reactions that are candid and you.
+                </p>
+                <p>
+                    Interested in just the engagement session without the <a
+                        href='galleries.php?w=24'>wedding photography</a>? That's alright
+                    too! <a href='/contact.php'>Contact me</a> today for more
+                    information.
+                </p>
+            </div>
+        </div>
+        <!-- /.row -->
 
         <?php require_once "../footer.php"; ?>
 
     </div>
-	<!-- /.container -->
+    <!-- /.container -->
 
     <?php
     if ($user->isAdmin ()) {
         ?>
     <script src="/js/edit-image.js"></script>
-	<script src="/js/jquery.uploadfile.js"></script>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"></script>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+    <script src="/js/jquery.uploadfile.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"></script>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
     <?php
     }
     ?>
