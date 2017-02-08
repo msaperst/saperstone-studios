2b384ed5aa93fd758f559f0731845f125447f2be
Max Saperstone <msaperst@gmail.com>
2017-02-08 13:39:01 -0500

    Adding GA to footer for production

--- footer.php
+++ footer.php
@@ -74,3 +74,23 @@
         duration: 2000, //changes the slide speed
     });
 </script>
+
+<!-- Google Analytics -->
+<?php
+if(isset($_SERVER['HTTPS'])) {
+    if ($_SERVER['HTTPS'] == "on") {
+?>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-23617021-6', 'auto');
+    ga('send', 'pageview');
+
+</script>
+<?php
+    }
+}
+?>
