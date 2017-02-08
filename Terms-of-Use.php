9e69ff42e12c64fa4798c38fd8197bba3ba6a152
Max Saperstone <msaperst@gmail.com>
2017-02-08 11:28:34 -0500

    Adding ToU page

---
+++ Terms-of-Use.php
@@ -0,0 +1,358 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php require_once "header.php"; ?>
+
+</head>
+
+<body>
+
+    <?php require_once "nav.php"; ?>
+
+    <!-- Page Content -->
+	<div class="page-content container">
+
+		<!-- Page Heading/Breadcrumbs -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header text-center">Terms of Use</h1>
+				<ol class="breadcrumb">
+					<li><a href="/">Home</a></li>
+					<li class="active">Information</li>
+					<li class="active">Terms</li>
+				</ol>
+			</div>
+		</div>
+		<!-- /.row -->
+
+		<!-- Content -->
+		<div class="row">
+			<div class="col-md-12">
+
+				<h4>Introduction</h4>
+				<p>These terms and conditions govern your use of this website; by
+					using this website, you accept these terms and conditions in full.
+					If you disagree with these terms and conditions or any part of
+					these terms and conditions, you must not use this website.</p>
+				<p>
+					This website uses cookies. By using this website and agreeing to
+					these terms and conditions, you consent to our SAPERSTONE STUDIOS's
+					use of cookies in accordance with the terms of SAPERSTONE STUDIOS's
+					<a href='Privacy-Policy.php'>privacy policy</a>.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>License to use website</h4>
+				<p>Unless otherwise stated, SAPERSTONE STUDIOS and/or its licensors
+					own the intellectual property rights in the website and material on
+					the website. Subject to the license below, all these intellectual
+					property rights are reserved.</p>
+				<p>You may download for caching purposes only, subject to the
+					restrictions set out below and elsewhere in these terms and
+					conditions.</p>
+				<p>You must not:
+				
+				
+				<ol>
+					<li>republish material from this website (including republication
+						on another website);</li>
+					<li>sell, rent or sub-license material from the website;</li>
+					<li>show any material from the website in public;</li>
+					<li>reproduce, duplicate, copy or otherwise exploit material on
+						this website for a commercial purpose;</li>
+					<li>edit or otherwise modify any material on the website; or</li>
+					<li>redistribute material from this website except for content
+						specifically and expressly made available for redistribution.</li>
+					<li>download or directly share images from this website except for
+						content specifically and expressly made available for
+						redistribution.</li>
+				</ol>
+				</p>
+				<p>Where content is specifically made available for redistribution,
+					it may only be redistributed based on specified terms.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Acceptable use</h4>
+				<p>You must not use this website in any way that causes, or may
+					cause, damage to the website or impairment of the availability or
+					accessibility of the website; or in any way which is unlawful,
+					illegal, fraudulent or harmful, or in connection with any unlawful,
+					illegal, fraudulent or harmful purpose or activity.</p>
+				<p>You must not use this website to copy, store, host, transmit,
+					send, use, publish or distribute any material which consists of (or
+					is linked to) any spyware, computer virus, Trojan horse, worm,
+					keystroke logger, rootkit or other malicious computer software.</p>
+				<p>You must not conduct any systematic or automated data collection
+					activities (including without limitation scraping, data mining,
+					data extraction and data harvesting) on or in relation to this
+					website without SAPERSTONE STUDIOS'S express written consent.</p>
+				<p>You must not use this website to transmit or send unsolicited
+					commercial communications.</p>
+				<p>You must not use this website for any purposes related to
+					marketing without SAPERSTONE STUDIOS'S express written consent.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Restricted access</h4>
+				<p>Access to certain areas of this website is restricted. SAPERSTONE
+					STUDIOS reserves the right to restrict access to other areas of
+					this website, or indeed this entire website, at SAPERSTONE
+					STUDIOS'S discretion.</p>
+				<p>If SAPERSTONE STUDIOS provides you with a user ID and password to
+					enable you to access restricted areas of this website or other
+					content or services, you must ensure that the user ID and password
+					are kept confidential.</p>
+				<p>SAPERSTONE STUDIOS may disable your user ID and password in
+					SAPERSTONE STUDIOS'S sole discretion without notice or explanation.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>User content</h4>
+				<p>In these terms and conditions, “your user content” means material
+					(including without limitation text, images, audio material, video
+					material and audio-visual material) that you submit to this
+					website, for whatever purpose.</p>
+				<p>You grant to SAPERSTONE STUDIOS a worldwide, irrevocable,
+					non-exclusive, royalty-free license to use, reproduce, adapt,
+					publish, translate and distribute your user content in any existing
+					or future media. You also grant to SAPERSTONE STUDIOS the right to
+					sub-license these rights, and the right to bring an action for
+					infringement of these rights.</p>
+				<p>Your user content must not be illegal or unlawful, must not
+					infringe any third party's legal rights, and must not be capable of
+					giving rise to legal action whether against you or SAPERSTONE
+					STUDIOS or a third party (in each case under any applicable law).</p>
+				<p>You must not submit any user content to the website that is or
+					has ever been the subject of any threatened or actual legal
+					proceedings or other similar complaint.</p>
+				<p>SAPERSTONE STUDIOS reserves the right to edit or remove any
+					material submitted to this website, or stored on SAPERSTONE
+					STUDIOS'S servers, or hosted or published upon this website.</p>
+				<p>Notwithstanding SAPERSTONE STUDIOS'S rights under these terms and
+					conditions in relation to user content, SAPERSTONE STUDIOS does not
+					undertake to monitor the submission of such content to, or the
+					publication of such content on, this website.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>No warranties</h4>
+				<p>This website is provided “as is” without any representations or
+					warranties, express or implied. SAPERSTONE STUDIOS makes no
+					representations or warranties in relation to this website or the
+					information and materials provided on this website.</p>
+				<p>Without prejudice to the generality of the foregoing paragraph,
+					SAPERSTONE STUDIOS does not warrant that:
+				
+				
+				<ul>
+					<li>this website will be constantly available, or available at all;
+						or</li>
+					<li>the information on this website is complete, true, accurate or
+						non-misleading.</li>
+				</ul>
+				</p>
+				<p>Nothing on this website constitutes, or is meant to constitute,
+					advice of any kind.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Limitations of liability</h4>
+				<p>SAPERSTONE STUDIOS will not be liable to you (whether under the
+					law of contact, the law of torts or otherwise) in relation to the
+					contents of, or use of, or otherwise in connection with, this
+					website:
+				
+				
+				<ul>
+					<li>to the extent that the website is provided free-of-charge, for
+						any direct loss;</li>
+					<li>for any indirect, special or consequential loss; or</li>
+					<li>for any business losses, loss of revenue, income, profits or
+						anticipated savings, loss of contracts or business relationships,
+						loss of reputation or goodwill, or loss or corruption of
+						information or data.</li>
+				</ul>
+				</p>
+				<p>These limitations of liability apply even if SAPERSTONE STUDIOS
+					has been expressly advised of the potential loss.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Exceptions</h4>
+				<p>Nothing in this website disclaimer will exclude or limit any
+					warranty implied by law that it would be unlawful to exclude or
+					limit; and nothing in this website disclaimer will exclude or limit
+					SAPERSTONE STUDIOS'S liability in respect of any:
+				
+				
+				<ul>
+					<li>death or personal injury caused by SAPERSTONE STUDIOS'S
+						negligence;</li>
+					<li>fraud or fraudulent misrepresentation on the part of SAPERSTONE
+						STUDIOS; or</li>
+					<li>matter which it would be illegal or unlawful for SAPERSTONE
+						STUDIOS to exclude or limit, or to attempt or purport to exclude
+						or limit, its liability.</li>
+				</ul>
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Reasonableness</h4>
+				<p>By using this website, you agree that the exclusions and
+					limitations of liability set out in this website disclaimer are
+					reasonable.</p>
+				<p>If you do not think they are reasonable, you must not use this
+					website.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Other parties</h4>
+				<p>You accept that, as a limited liability entity, SAPERSTONE
+					STUDIOS has an interest in limiting the personal liability of its
+					officers and employees. You agree that you will not bring any claim
+					personally against SAPERSTONE STUDIOS'S officers or employees in
+					respect of any losses you suffer in connection with the website.</p>
+				<p>Without prejudice to the foregoing paragraph, you agree that the
+					limitations of warranties and liability set out in this website
+					disclaimer will protect SAPERSTONE STUDIOS'S officers, employees,
+					agents, subsidiaries, successors, assigns and sub-contractors as
+					well as SAPERSTONE STUDIOS.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Unenforceable provisions</h4>
+				<p>If any provision of this website disclaimer is, or is found to
+					be, unenforceable under applicable law, that will not affect the
+					enforceability of the other provisions of this website disclaimer.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Indemnity</h4>
+				<p>You hereby indemnify SAPERSTONE STUDIOS and undertake to keep
+					SAPERSTONE STUDIOS indemnified against any losses, damages, costs,
+					liabilities and expenses (including without limitation legal
+					expenses and any amounts paid by SAPERSTONE STUDIOS to a third
+					party in settlement of a claim or dispute on the advice of
+					SAPERSTONE STUDIOS'S legal advisers) incurred or suffered by
+					SAPERSTONE STUDIOS arising out of any breach by you of any
+					provision of these terms and conditions, or arising out of any
+					claim that you have breached any provision of these terms and
+					conditions.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Breaches of these terms and conditions</h4>
+				<p>Without prejudice to SAPERSTONE STUDIOS'S other rights under
+					these terms and conditions, if you breach these terms and
+					conditions in any way, SAPERSTONE STUDIOS may take such action as
+					SAPERSTONE STUDIOS deems appropriate to deal with the breach,
+					including suspending your access to the website, prohibiting you
+					from accessing the website, blocking computers using your IP
+					address from accessing the website, contacting your internet
+					service provider to request that they block your access to the
+					website and/or bringing court proceedings against you.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Variation</h4>
+				<p>SAPERSTONE STUDIOS may revise these terms and conditions from
+					time-to-time. Revised terms and conditions will apply to the use of
+					this website from the date of the publication of the revised terms
+					and conditions on this website. Please check this page regularly to
+					ensure you are familiar with the current version.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Assignment</h4>
+				<p>SAPERSTONE STUDIOS may transfer, sub-contract or otherwise deal
+					with SAPERSTONE STUDIOS'S rights and/or obligations under these
+					terms and conditions without notifying you or obtaining your
+					consent.</p>
+				<p>You may not transfer, sub-contract or otherwise deal with your
+					rights and/or obligations under these terms and conditions.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Severability</h4>
+				<p>If a provision of these terms and conditions is determined by any
+					court or other competent authority to be unlawful and/or
+					unenforceable, the other provisions will continue in effect. If any
+					unlawful and/or unenforceable provision would be lawful or
+					enforceable if part of it were deleted, that part will be deemed to
+					be deleted, and the rest of the provision will continue in effect.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Entire agreement</h4>
+				<p>These terms and conditions constitute the entire agreement
+					between you and SAPERSTONE STUDIOS in relation to your use of this
+					website, and supersede all previous agreements in respect of your
+					use of this website.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Law and jurisdiction</h4>
+				<p>These terms and conditions will be governed by and construed in
+					accordance with VIRGINIA STATE LAW, and any disputes relating to
+					these terms and conditions will be subject to the non-exclusive
+					jurisdiction of the courts of FAIRFAX.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>SAPERSTONE STUDIOS'S details</h4>
+				<p>The full name of SAPERSTONE STUDIOS is SAPERSTONE STUDIOS LLC.</p>
+				<p>SAPERSTONE STUDIOS is registered in FAIRFAX, VA under
+					registration number 46-2778744.</p>
+				<p>SAPERSTONE STUDIOS'S registered address is 5012 Whisper Willow
+					Dr, Fairfax, VA 22030.</p>
+				<p>
+					You can contact SAPERSTONE STUDIOS by email to <a target='_blank'
+						href='mailto:contact@saperstonestudios.com'>contact@saperstonestudios.com</a>.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h4>Credit</h4>
+				<p>
+					This document was created using a Contractology template available
+					at <a target='_blank' href='http://www.contractology.com'>http://www.contractology.com</a>.
+				</p>
+			</div>
+		</div>
+		<!-- /.row -->
+
+        <?php require_once "footer.php"; ?>
+
+    </div>
+	<!-- /.container -->
+
+</body>
+
+</html>
