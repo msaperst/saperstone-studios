4c598dc0b159a71b379c1c3bb092340a54e7db4d
Max Saperstone <msaperst@gmail.com>
2017-02-19 22:27:50 -0500

    Fixing some sql injection vulnerabilities by parsing out inputs

--- blog/post.php
+++ blog/post.php
@@ -15,7 +15,7 @@ if (! isset ( $_GET ['p'] )) {
     include "../errors/404.php";
     exit ();
 } else {
-    $post = $_GET ['p'];
+    $post = ( int ) $_GET ['p'];
 }
 
 require_once "../php/sql.php";
@@ -55,105 +55,105 @@ if (! $user->isAdmin () && ! $details ['active']) {
     <?php require_once "../nav.php"; ?>
     
     <!-- Page Content -->
-    <div class="page-content container">
+	<div class="page-content container">
 
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header text-center">Recent Blog Posts</h1>
-                <ol class="breadcrumb">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/blog/">Blog</a></li>
-                    <li><a href="/blog/posts.php">Posts</a></li>
-                    <li class="active" id="breadcrumb-title"></li>
+		<!-- Page Heading/Breadcrumbs -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header text-center">Recent Blog Posts</h1>
+				<ol class="breadcrumb">
+					<li><a href="/">Home</a></li>
+					<li><a href="/blog/">Blog</a></li>
+					<li><a href="/blog/posts.php">Posts</a></li>
+					<li class="active" id="breadcrumb-title"></li>
                     <?php
                     if ($user->isAdmin ()) {
                         ?>
                     <li class="no-before pull-right"><button
-                            type="button" id="edit-post-btn" class="btn btn-xs btn-warning"
-                            data-toggle="tooltip" data-placement="left"
-                            title="Edit Blog Post">
-                            <i class="fa fa-pencil-square-o"></i>
-                        </button></li>
+							type="button" id="edit-post-btn" class="btn btn-xs btn-warning"
+							data-toggle="tooltip" data-placement="left"
+							title="Edit Blog Post">
+							<i class="fa fa-pencil-square-o"></i>
+						</button></li>
                     <?php
                     }
                     ?>
                 </ol>
-            </div>
-        </div>
-        <!-- /.row -->
+			</div>
+		</div>
+		<!-- /.row -->
 
-        <!-- Post Section -->
-        <div id="post-content"></div>
-        <!-- /.row -->
+		<!-- Post Section -->
+		<div id="post-content"></div>
+		<!-- /.row -->
 
-        <!-- Comment Section -->
-        <div class="row">
-            <div class="col-md-12">
-                <h2 class='text-center'>Leave A Comment</h2>
-            </div>
-        </div>
-        <div class="row">
-            <input type="text" class="hidden" id="post-comment-user"
-                value="<?php echo $user->getId(); ?>" />
-            <div class="col-md-1">
-                <label for="post-comment-name">Name:</label>
-            </div>
-            <div class="col-md-5">
-                <input type="text" class="form-control" id="post-comment-name"
-                    placeholder="Name" value="<?php echo $user->getName(); ?>" />
-                <div class="error" id="post-comment-name-message"></div>
-            </div>
-            <div class="col-md-1">
-                <label for="post-comment-email">Email:</label>
-            </div>
-            <div class="col-md-5">
-                <input type="text" class="form-control" id="post-comment-email"
-                    placeholder="Email"
-                    data-validation-required-message="Please enter a valid email address"
-                    value="<?php echo $user->getEmail(); ?>" />
-                <div class="error" id="post-comment-email-message"></div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="controls">
-                    <textarea rows="5    " cols="100" class="form-control"
-                        id="post-comment-message" placeholder="Message" required=""
-                        data-validation-required-message="Please enter your message"
-                        maxlength="999" style="resize: none" aria-invalid="false"></textarea>
-                    <div class="error" id="post-comment-message-message"></div>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
+		<!-- Comment Section -->
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class='text-center'>Leave A Comment</h2>
+			</div>
+		</div>
+		<div class="row">
+			<input type="text" class="hidden" id="post-comment-user"
+				value="<?php echo $user->getId(); ?>" />
+			<div class="col-md-1">
+				<label for="post-comment-name">Name:</label>
+			</div>
+			<div class="col-md-5">
+				<input type="text" class="form-control" id="post-comment-name"
+					placeholder="Name" value="<?php echo $user->getName(); ?>" />
+				<div class="error" id="post-comment-name-message"></div>
+			</div>
+			<div class="col-md-1">
+				<label for="post-comment-email">Email:</label>
+			</div>
+			<div class="col-md-5">
+				<input type="text" class="form-control" id="post-comment-email"
+					placeholder="Email"
+					data-validation-required-message="Please enter a valid email address"
+					value="<?php echo $user->getEmail(); ?>" />
+				<div class="error" id="post-comment-email-message"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="controls">
+					<textarea rows="5    " cols="100" class="form-control"
+						id="post-comment-message" placeholder="Message" required=""
+						data-validation-required-message="Please enter your message"
+						maxlength="999" style="resize: none" aria-invalid="false"></textarea>
+					<div class="error" id="post-comment-message-message"></div>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
 
-        <!-- View Comments -->
-        <div id="post-comments">
-            <div class="row">
-                <div class="col-md-6">
-                    <h2 class='text-left'></h2>
-                </div>
-                <div class="col-md-6 text-right">
-                    <button post-id="<?php echo $post; ?>" id="post-comment-submit"
-                        class="btn btn-success disabled" style="margin-top: 20px;"
-                        disabled>Submit Comment</button>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
+		<!-- View Comments -->
+		<div id="post-comments">
+			<div class="row">
+				<div class="col-md-6">
+					<h2 class='text-left'></h2>
+				</div>
+				<div class="col-md-6 text-right">
+					<button post-id="<?php echo $post; ?>" id="post-comment-submit"
+						class="btn btn-success disabled" style="margin-top: 20px;"
+						disabled>Submit Comment</button>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
 
         <?php require_once "../footer.php"; ?>
 
     </div>
-    <!-- /.container -->
+	<!-- /.container -->
 
-    <!-- Gallery JavaScript -->
-    <script src="/js/post.js"></script>
-    <script src="/js/post-full.js"></script>
+	<!-- Gallery JavaScript -->
+	<script src="/js/post.js"></script>
+	<script src="/js/post-full.js"></script>
 
-    <!-- Script to Activate the Gallery -->
-    <script>
+	<!-- Script to Activate the Gallery -->
+	<script>
         new PostFull( <?php echo $post; ?> );
     </script>
 
--- reviews.php
+++ reviews.php
@@ -6,7 +6,7 @@ $conn = new Sql ();
 $conn->connect ();
 
 if (isset ( $_GET ['c'] )) {
-    $category = $_GET ['c'];
+    $category = ( int ) $_GET ['c'];
     $sql = "SELECT * FROM `review_types` WHERE id = '$category';";
     $details = mysqli_fetch_assoc ( mysqli_query ( $conn->db, $sql ) );
     if (! $details ['name']) {
@@ -48,39 +48,39 @@ if (isset ( $_GET ['c'] )) {
     ?>
 
     <!-- Page Content -->
-    <div class="page-content container">
+	<div class="page-content container">
 
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header text-center">
+		<!-- Page Heading/Breadcrumbs -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header text-center">
                 <?php
                 if (isset ( $category )) {
                     echo ucfirst ( $details ['name'] ) . " ";
                 }
                 ?>
                 Raves</h1>
-                <ol class="breadcrumb">
-                    <li><a href="/">Home</a></li>
+				<ol class="breadcrumb">
+					<li><a href="/">Home</a></li>
                     <?php
                     if (isset ( $category )) {
                         ?>
                     <li><a href='index.php'><?php  echo ucfirst ( $details ['name'] ); ?>s</a></li>
-                    <li class='active'>Raves</li>
+					<li class='active'>Raves</li>
                     <?php
                     } else {
                         ?>
                     <li class="active">Information</li>
-                    <li class='active'>Raves</li>
+					<li class='active'>Raves</li>
                     <?php
                     }
                     ?>
                 </ol>
-            </div>
-        </div>
-        <!-- /.row -->
+			</div>
+		</div>
+		<!-- /.row -->
 
-        <div class="row">
+		<div class="row">
             <?php
             $where = "";
             if (isset ( $category )) {
@@ -96,98 +96,98 @@ if (isset ( $_GET ['c'] )) {
                 }
                 ?>
             <div class="col-xs-6">
-                <div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-                    <img src="<?php echo $r['image1']; echo $rand; ?>" width="100%"
-                        alt="<?php echo $r['image1']; ?>">
-                </div>
-            </div>
-            <div class="col-xs-6">
-                <div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-                    <img src="<?php echo $r['image2']; echo $rand; ?>" width="100%"
-                        alt="<?php echo $r['image2']; ?>">
-                </div>
-            </div>
-            <div class="col-xs-12" style="padding-top: 20px;">
+				<div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+					<img src="<?php echo $r['image1']; echo $rand; ?>" width="100%"
+						alt="<?php echo $r['image1']; ?>">
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+					<img src="<?php echo $r['image2']; echo $rand; ?>" width="100%"
+						alt="<?php echo $r['image2']; ?>">
+				</div>
+			</div>
+			<div class="col-xs-12" style="padding-top: 20px;">
 
-                <blockquote>
-                    <p>
+				<blockquote>
+					<p>
                         <?php echo $r['quote']; ?>
                     
                     </p>
-                    <footer><?php echo $r['client']; ?><br /> <em><?php echo $r['event']; ?></em>
-                    </footer>
+					<footer><?php echo $r['client']; ?><br /> <em><?php echo $r['event']; ?></em>
+					</footer>
 
-                </blockquote>
-                <hr />
-            </div>
+				</blockquote>
+				<hr />
+			</div>
             <?php
                 $counter ++;
             }
             ?>
         </div>
-        <!-- /.row -->
+		<!-- /.row -->
 
-        <!-- Links to external sites -->
-        <div class="row">
-            <div class="col-lg-12">
-                <p>Think these reviews are great? See where else people are saying
-                    fantastic things about Saperstone Studios.</p>
-            </div>
+		<!-- Links to external sites -->
+		<div class="row">
+			<div class="col-lg-12">
+				<p>Think these reviews are great? See where else people are saying
+					fantastic things about Saperstone Studios.</p>
+			</div>
 
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div section='Wedding Wire'
-                    class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-                    <span class='preview-title'>Wedding Wire</span> <img
-                        class="img-responsive"
-                        src="/img/wedding-wire.jpg<?php echo $rand; ?>" width="100%"
-                        alt="Wedding Wire">
-                    <div class="overlay">
-                        <br /> <br /> <br /> <a class="info" target="_blank"
-                            href="http://www.weddingwire.com/reviews/saperstone-studios-reston/cdbd87c3e3540e8e.html">See
-                            More</a>
-                    </div>
-                </div>
-            </div>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div section='Wedding Wire'
+					class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+					<span class='preview-title'>Wedding Wire</span> <img
+						class="img-responsive"
+						src="/img/wedding-wire.jpg<?php echo $rand; ?>" width="100%"
+						alt="Wedding Wire">
+					<div class="overlay">
+						<br /> <br /> <br /> <a class="info" target="_blank"
+							href="http://www.weddingwire.com/reviews/saperstone-studios-reston/cdbd87c3e3540e8e.html">See
+							More</a>
+					</div>
+				</div>
+			</div>
 
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div section='Yelp'
-                    class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-                    <span class='preview-title'>Yelp</span> <img class="img-responsive"
-                        src="/img/yelp.jpg<?php echo $rand; ?>" width="100%" alt="Yelp">
-                    <div class="overlay">
-                        <br /> <br /> <br /> <a class="info" target="_blank"
-                            href="http://www.yelp.com/biz/saperstone-studios-fairfax">See
-                            More</a>
-                    </div>
-                </div>
-            </div>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div section='Yelp'
+					class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+					<span class='preview-title'>Yelp</span> <img class="img-responsive"
+						src="/img/yelp.jpg<?php echo $rand; ?>" width="100%" alt="Yelp">
+					<div class="overlay">
+						<br /> <br /> <br /> <a class="info" target="_blank"
+							href="http://www.yelp.com/biz/saperstone-studios-fairfax">See
+							More</a>
+					</div>
+				</div>
+			</div>
 
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div section='Google'
-                    class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
-                    <span class='preview-title'>Google</span> <img
-                        class="img-responsive" src="/img/google.jpg<?php echo $rand; ?>"
-                        width="100%" alt="Google">
-                    <div class="overlay">
-                        <br /> <br /> <br /> <a class="info" target="_blank"
-                            href="https://www.google.com/search?q=saperstone+studios&oq=saperstone+studios&aqs=chrome..69i57j69i60l2j69i65.2255j0j4&sourceid=chrome&ie=UTF-8#lrd=0x89b637e9f071197b:0x4c75c5462bac5863,1,">See
-                            More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div section='Google'
+					class="hovereffect img-portfolio<?php if ($user->isAdmin ()) { echo " editable"; } ?>">
+					<span class='preview-title'>Google</span> <img
+						class="img-responsive" src="/img/google.jpg<?php echo $rand; ?>"
+						width="100%" alt="Google">
+					<div class="overlay">
+						<br /> <br /> <br /> <a class="info" target="_blank"
+							href="https://www.google.com/search?q=saperstone+studios&oq=saperstone+studios&aqs=chrome..69i57j69i60l2j69i65.2255j0j4&sourceid=chrome&ie=UTF-8#lrd=0x89b637e9f071197b:0x4c75c5462bac5863,1,">See
+							More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
 
         <?php require_once "footer.php"; ?>
 
     </div>
-    <!-- /.container -->
+	<!-- /.container -->
  
  <?php
 if ($user->isAdmin ()) {
     ?>
     <script src="/js/edit-image.js"></script>
-    <script src="/js/jquery.uploadfile.js"></script>
+	<script src="/js/jquery.uploadfile.js"></script>
     <?php
 }
 ?>
