6845691050a7bc68391e7a31cf5d63af736e9f73
Max Saperstone <msaperst@gmail.com>
2017-03-20 15:51:56 -0400

    Adding additional product galleries

--- js/contract.js
+++ js/contract.js
@@ -15,8 +15,61 @@ $(document).ready(function() {
     $('#contract-clear-signature').click(function() {
         signature.jSignature("reset");
     });
-    
+
     $('#contract-clear-initial').click(function() {
         initial.jSignature("reset");
     });
+
+    $('input,textarea,#contract-signature,#contract-initial').change(function() {
+        checkInputs();
+    });
+    
+    $('#contract-submit').click(function(){
+        submitContract();
+    });
 });
+
+function checkInputs() {
+    var okToSubmit = true;
+    $('input.keep,textarea.keep').each(function() {
+        if ($(this).val() === "") {
+            okToSubmit = false;
+        }
+    });
+    if (signature.jSignature("getData", "native").length === 0 || initial.jSignature("getData", "native").length === 0) {
+        okToSubmit = false;
+    }
+    if (okToSubmit) {
+        $('#contract-submit').removeClass('disabled').prop('disabled', false);
+    } else {
+        $('#contract-submit').addClass('disabled').prop('disabled', true);
+    }
+    return okToSubmit;
+}
+
+function submitContract() {
+    if ( ! checkInputs() ) {
+        BootstrapDialog.alert('Please finish signing the contract before submitting.');
+    }
+    $.post('/api/sign-contract.php', {
+        id : id,
+        name : name,
+        address : address,
+        number : number,
+        email : email,
+        signature : signature,
+        initial : signature,
+    }).done(function(data) {
+        if (data !== "") {
+            modal.find('.bootstrap-dialog-body').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>" + data + "</div>");
+        } else {
+            dialogItself.close();
+        }
+        contract_table.ajax.reload(null, false);
+    }).fail(function() {
+        modal.find('.bootstrap-dialog-body').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Some unexpected error occurred while updating your user's albums.<br/>Please <a class='gen' target='_blank' href='mailto:admin@saperstonestudios.com'>Contact our System Administrators</a> for more details, or try resubmitting.</div>");
+    }).always(function() {
+        button.stopSpin();
+        enableDialogButtons(dialogItself);
+    });
+}
