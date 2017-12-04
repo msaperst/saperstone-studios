84c2db29a2db8bbd42987f83c6e17b60a3407ed2
Max Saperstone <msaperst@gmail.com>
2017-12-04 01:38:55 -0500

    Fixing mysql issue

--- templates/nav.php
+++ templates/nav.php
@@ -10,14 +10,14 @@ $result = mysqli_query ( $conn->db, $sql );
 if (mysqli_num_rows ( $result )) {
     ?>
 <div id='displayed-alerts'
-    style='position: fixed; width: 100%; top: -10px; z-index: 10000;'>
+	style='position: fixed; width: 100%; top: -10px; z-index: 10000;'>
     <?php
 }
 while ( $row = mysqli_fetch_assoc ( $result ) ) {
     if (! isset ( $_COOKIE ["id" . $row ['id']] )) {
         ?>
 <div
-        class="alert alert-warning<?php if ($row['dismissible']) { echo " alert-dismissable fade in"; } ?>">
+		class="alert alert-warning<?php if ($row['dismissible']) { echo " alert-dismissable fade in"; } ?>">
         <?php if ($row['dismissible']) { echo "<a href='#' onclick='javascript:setCookie(\"id${row['id']}=dismiss\", 9999999);' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>"; } ?>
         <strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong> <?php echo $row['message']; ?>
 </div>
@@ -29,27 +29,26 @@ if (mysqli_num_rows ( $result )) {
     </div>
 <?php
 }
-$conn->disconnect ();
 ?>
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span> <span
-                    class="icon-bar"></span> <span class="icon-bar"></span> <span
-                    class="icon-bar"></span>
-            </button>
-            <a id="nav-logo-link" class="nav-logo-link navbar-brand" href="/"><img
-                class="nav-logo" id="nav-logo" src="/img/2014websitelogo250px.png"
-                alt="Saperstone Studios Logo"></a> <a id="nav-logo-link-2"
-                class="nav-logo-link navbar-brand" href="/"><img class="nav-logo"
-                id="nav-logo-2" src="/img/2014websitelogo250px.png"
-                alt="Saperstone Studios Logo"></a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a id="nav-logo-link" class="nav-logo-link navbar-brand" href="/"><img
+				class="nav-logo" id="nav-logo" src="/img/2014websitelogo250px.png"
+				alt="Saperstone Studios Logo"></a> <a id="nav-logo-link-2"
+				class="nav-logo-link navbar-brand" href="/"><img class="nav-logo"
+				id="nav-logo-2" src="/img/2014websitelogo250px.png"
+				alt="Saperstone Studios Logo"></a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
         
         <?php
         switch ($nav) {
@@ -71,115 +70,115 @@ $conn->disconnect ();
                 if (! $user->isLoggedIn ()) {
                     ?>
                 <li><a href="javascript:void(0);" data-toggle="modal"
-            data-target="#login-modal"><em class="fa fa-sign-in"></em> Login</a></li>
+			data-target="#login-modal"><em class="fa fa-sign-in"></em> Login</a></li>
                 <?php
                 } elseif ($user->isAdmin ()) {
                     ?>
                 <li class="dropdown"><a href="javascript:void(0);"
-            class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->getUser (); ?><strong
-                class="caret"></strong></a>
-            <ul class="dropdown-menu">
-                <li><a href="/user/users.php">Manage Users</a></li>
-                <li><a href="/user/index.php">Manage Albums</a></li>
-                <li><a href="/user/products.php">Manage Products</a></li>
-                <li><a href="/user/contracts.php">Manage Contracts</a></li>
-                <li><a href="/user/usage.php">View Usage</a></li>
-                <li><a href="/user/profile.php">Manage Profile</a></li>
-                <li><a id='logout-button' href="javascript:void(0);"><em
-                        class="fa fa-sign-out"></em> Logout</a></li>
-            </ul></li>
+			class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->getUser (); ?><strong
+				class="caret"></strong></a>
+			<ul class="dropdown-menu">
+				<li><a href="/user/users.php">Manage Users</a></li>
+				<li><a href="/user/index.php">Manage Albums</a></li>
+				<li><a href="/user/products.php">Manage Products</a></li>
+				<li><a href="/user/contracts.php">Manage Contracts</a></li>
+				<li><a href="/user/usage.php">View Usage</a></li>
+				<li><a href="/user/profile.php">Manage Profile</a></li>
+				<li><a id='logout-button' href="javascript:void(0);"><em
+						class="fa fa-sign-out"></em> Logout</a></li>
+			</ul></li>
                    <?php
                 } else {
                     ?>
                 <li class="dropdown"><a href="javascript:void(0);"
-            class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->getUser (); ?><strong
-                class="caret"></strong></a>
-            <ul class="dropdown-menu">
-                <li><a href="/user/index.php">View Albums</a></li>
-                <li><a href="/user/profile.php">Manage Profile</a></li>
-                <li><a id='logout-button' href="javascript:void(0);"><em
-                        class="fa fa-sign-out"></em> Logout</a></li>
-            </ul></li>
+			class="dropdown-toggle" data-toggle="dropdown"><?php echo $user->getUser (); ?><strong
+				class="caret"></strong></a>
+			<ul class="dropdown-menu">
+				<li><a href="/user/index.php">View Albums</a></li>
+				<li><a href="/user/profile.php">Manage Profile</a></li>
+				<li><a id='logout-button' href="javascript:void(0);"><em
+						class="fa fa-sign-out"></em> Logout</a></li>
+			</ul></li>
                    <?php
                 }
                 ?>
 
             </ul>
-    </div>
-    <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
+	</div>
+	<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
 </nav>
 <!-- Div to fix spacing -->
 <div id="nav-spacer"></div>
 
 <div id="login-modal" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-sm">
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Login to Your Account</h4>
-            </div>
-            <form>
-                <div class="modal-body">
-                    <input class="form-control" id="login-user" type="text" name="user"
-                        placeholder="Username" /> <input class="form-control"
-                        id="login-pass" type="password" name="pass" placeholder="Password" />
-                    <span>
-                        <div class="checkbox">
-                            <label><input type="checkbox"> Remember me</label>
-                        </div>
-                    </span>
-                    <div class="login-help">
-                        <a href="/register.php">Register</a> - <a
-                            id="login-forgot-password" href="javascript:void(0);">Forgot
-                            Username/Password</a>
-                    </div>
-                </div>
-            </form>
-            <div class="modal-footer">
-                <button id="login-submit" type="submit" class="btn btn-primary">Login</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
+	<div class="modal-dialog modal-sm">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Login to Your Account</h4>
+			</div>
+			<form>
+				<div class="modal-body">
+					<input class="form-control" id="login-user" type="text" name="user"
+						placeholder="Username" /> <input class="form-control"
+						id="login-pass" type="password" name="pass" placeholder="Password" />
+					<span>
+						<div class="checkbox">
+							<label><input type="checkbox"> Remember me</label>
+						</div>
+					</span>
+					<div class="login-help">
+						<a href="/register.php">Register</a> - <a
+							id="login-forgot-password" href="javascript:void(0);">Forgot
+							Username/Password</a>
+					</div>
+				</div>
+			</form>
+			<div class="modal-footer">
+				<button id="login-submit" type="submit" class="btn btn-primary">Login</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div id="forgot-password-modal" class="modal fade" role="dialog">
-    <div class="modal-dialog modal-sm">
-        <!-- Modal content-->
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Forgotten Credentials</h4>
-            </div>
-            <div class="modal-body">
-                <input class="form-control" id="forgot-password-email" type="email"
-                    name="email" placeholder="Email" />
-                <div id="forgot-password-instructions">
-                    A reset code will be sent to your email address. If you already
-                    have one, <a id="forgot-password-prev-code"
-                        href="javascript:void(0);">click here</a>
-                </div>
-                <input class="form-control" id="forgot-password-code" type="text"
-                    name="code" placeholder="Reset Code" style="display: none;" /> <input
-                    class="form-control" id="forgot-password-new-password"
-                    type="password" name="pass" placeholder="Password"
-                    style="display: none;" /> <input class="form-control"
-                    id="forgot-password-new-password-confirm" type="password"
-                    name="pass-conf" placeholder="Re-type Password"
-                    style="display: none;" />
-            </div>
-            <div class="modal-footer">
-                <button id="forgot-password-submit" type="submit"
-                    class="btn btn-primary">Send Code</button>
-                <button id="forgot-password-reset-password" type="submit"
-                    class="btn btn-primary" style="display: none;">Reset Password</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
+	<div class="modal-dialog modal-sm">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Forgotten Credentials</h4>
+			</div>
+			<div class="modal-body">
+				<input class="form-control" id="forgot-password-email" type="email"
+					name="email" placeholder="Email" />
+				<div id="forgot-password-instructions">
+					A reset code will be sent to your email address. If you already
+					have one, <a id="forgot-password-prev-code"
+						href="javascript:void(0);">click here</a>
+				</div>
+				<input class="form-control" id="forgot-password-code" type="text"
+					name="code" placeholder="Reset Code" style="display: none;" /> <input
+					class="form-control" id="forgot-password-new-password"
+					type="password" name="pass" placeholder="Password"
+					style="display: none;" /> <input class="form-control"
+					id="forgot-password-new-password-confirm" type="password"
+					name="pass-conf" placeholder="Re-type Password"
+					style="display: none;" />
+			</div>
+			<div class="modal-footer">
+				<button id="forgot-password-submit" type="submit"
+					class="btn btn-primary">Send Code</button>
+				<button id="forgot-password-reset-password" type="submit"
+					class="btn btn-primary" style="display: none;">Reset Password</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
 </div>
 
 </div>
