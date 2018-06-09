cc1a3b5639ada5ee75a2a6d076a02ba1f270f4a1
Max Saperstone <msaperst@gmail.com>
2018-06-08 21:40:34 -0400

    Fixing icon size

--- templates/social-list.php
+++ templates/social-list.php
@@ -1,12 +1,12 @@
 <li><a target="_blank"
     href="https://www.facebook.com/SaperstoneStudios"><em
-        class="fa fa-facebook-square <? echo $iconSize; ?>"></em></a></li>
+        class="fa fa-facebook-square <?php echo $iconSize; ?>"></em></a></li>
 <li><a target="_blank" href="https://instagram.com/saperstonestudios"><em
-        class="fa fa-instagram <? echo $iconSize; ?>"></em></a></li>
+        class="fa fa-instagram <?php echo $iconSize; ?>"></em></a></li>
 <li><a target="_blank" href="https://twitter.com/LaSaperstone"><em
-        class="fa fa-twitter-square <? echo $iconSize; ?>"></em></a></li>
+        class="fa fa-twitter-square <?php echo $iconSize; ?>"></em></a></li>
 <li><a target="_blank" type="application/rss+xml" href="/blog.rss"><em
-        class="fa fa-rss-square <? echo $iconSize; ?>"></em></a></li>
+        class="fa fa-rss-square <?php echo $iconSize; ?>"></em></a></li>
 <li><a target="_blank"
     href="https://plus.google.com/+SaperstoneStudios"><em
-        class="fa fa-google-plus-square <? echo $iconSize; ?>"></em></a></li>
+        class="fa fa-google-plus-square <?php echo $iconSize; ?>"></em></a></li>
