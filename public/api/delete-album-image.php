1992291f8cc870ab4ba5ee8902b1635fb495ce76
Max Saperstone <msaperstone@testpros.com>
2020-07-31 07:51:47 -0400

    Fixing and writing tests for delete-album-image api

--- public/api/delete-album-image.php
+++ public/api/delete-album-image.php
@@ -3,7 +3,6 @@ require_once dirname ( $_SERVER ['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . "src
 require_once dirname ( $_SERVER ['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . "src/session.php";
 require_once dirname ( $_SERVER ['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . "src/user.php";
 $sql = new Sql ();
-
 $user = new User ($sql);
 
 if (! $user->isAdmin ()) {
@@ -11,7 +10,7 @@ if (! $user->isAdmin ()) {
     if ($user->isLoggedIn ()) {
         echo "You do not have appropriate rights to perform this action";
     }
-    $conn->disconnect ();
+    $sql->disconnect ();
     exit ();
 }
 
@@ -20,21 +19,20 @@ if (isset ( $_POST ['album'] ) && $_POST ['album'] != "") {
     $album = ( int ) $_POST ['album'];
 } else {
     if (! isset ( $_POST ['album'] )) {
-        echo "Album id is required!";
-    } elseif ($_POST ['album'] != "") {
-        echo "Album id cannot be blank!";
+        echo "Album id is required";
+    } elseif ($_POST ['album'] == "") {
+        echo "Album id can not be blank";
     } else {
-        echo "Some other Album id error occurred!";
+        echo "Some other album id error occurred";
     }
-    $conn->disconnect ();
+    $sql->disconnect ();
     exit ();
 }
 
-$sql = "SELECT * FROM albums WHERE id = $album;";
-$album_info = $sql->getRow( $sql );
+$album_info = $sql->getRow( "SELECT * FROM albums WHERE id = $album;" );
 if (! $album_info ['id']) {
-    echo "That ID doesn't match any albums";
-    $conn->disconnect ();
+    echo "Album id does not match any albums";
+    $sql->disconnect ();
     exit ();
 }
 
@@ -43,50 +41,39 @@ if (isset ( $_POST ['image'] ) && $_POST ['image'] != "") {
     $sequence = ( int ) $_POST ['image'];
 } else {
     if (! isset ( $_POST ['image'] )) {
-        echo "Image id is required!";
-    } elseif ($_POST ['image'] != "") {
-        echo "Image id cannot be blank!";
+        echo "Image id is required";
+    } elseif ($_POST ['image'] == "") {
+        echo "Image id can not be blank";
     } else {
-        echo "Some other Image id error occurred!";
+        echo "Some other image id error occurred";
     }
-    $conn->disconnect ();
+    $sql->disconnect ();
     exit ();
 }
 
 // delete our image from mysql table
-$sql = "SELECT location FROM album_images WHERE album='$album' AND sequence='$sequence';";
-$row = $sql->getRow( $sql );
-$sql = "DELETE FROM album_images WHERE album='$album' AND sequence='$sequence';";
-mysqli_query ( $conn->db, $sql );
-
+$row = $sql->getRow( "SELECT location FROM album_images WHERE album='$album' AND sequence='$sequence';" );
+$sql->executeStatement( "DELETE FROM album_images WHERE album='$album' AND sequence='$sequence';" );
 // update the image count
-$sql = "UPDATE albums SET images = images - 1 WHERE id='$album';";
-mysqli_query ( $conn->db, $sql );
+$sql->executeStatement( "UPDATE albums SET images = images - 1 WHERE id='$album';" );
 
 // need to re-sequence images in mysql table, and make these updates in cart, download rights, favorites, and share rights to match
-$sql = "ALTER TABLE album_images ADD COLUMN new_sequence INT;";
-mysqli_query ( $conn->db, $sql );
-$sql = "SET @seq:=-1;";
-mysqli_query ( $conn->db, $sql );
-$sql = "UPDATE album_images SET new_sequence=(@seq:=@seq+1) WHERE album='$album';";
-mysqli_query ( $conn->db, $sql );
-$sql = "SELECT * FROM album_images WHERE album='$album';";
-$result = mysqli_query ( $conn->db, $sql );
-while ( $r = mysqli_fetch_assoc ( $result ) ) {
+$sql->executeStatement( "ALTER TABLE album_images ADD COLUMN new_sequence INT;" );
+$sql->executeStatement( "SET @seq:=-1;" );
+$sql->executeStatement( "UPDATE album_images SET new_sequence=(@seq:=@seq+1) WHERE album='$album';" );
+$result = $sql->getRows( "SELECT * FROM album_images WHERE album='$album';" );
+foreach ( $result as $r ) {
     foreach ( array (
             'cart',
             'download_rights',
             'favorites',
             'share_rights' 
     ) as $table ) {
-        $sql = "UPDATE $table SET image=${r['new_sequence']} WHERE album='$album' AND image='${r['sequence']}';";
-        mysqli_query ( $conn->db, $sql );
+        $sql->executeStatement( "UPDATE $table SET image=${r['new_sequence']} WHERE album='$album' AND image='${r['sequence']}';" );
     }
-    $sql = "UPDATE album_images SET sequence=${r['new_sequence']} WHERE album='$album' AND new_sequence='${r['new_sequence']}';";
-    mysqli_query ( $conn->db, $sql );
+    $sql->executeStatement( "UPDATE album_images SET sequence=${r['new_sequence']} WHERE album='$album' AND new_sequence='${r['new_sequence']}';" );
 }
-$sql = "ALTER TABLE album_images DROP COLUMN new_sequence;";
-mysqli_query ( $conn->db, $sql );
+$sql->executeStatement( "ALTER TABLE album_images DROP COLUMN new_sequence;" );
 
 // delete our image from the file system
 if ($row ['location'] != "") {
@@ -96,5 +83,5 @@ if ($row ['location'] != "") {
     system ( "rm -f " . escapeshellarg ( "../" . implode ( "/", $parts ) ) );
 }
 
-$conn->disconnect ();
+$sql->disconnect ();
 exit ();
---
+++ tests/api/DeleteAlbumImageTest.php
@@ -0,0 +1,189 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+
+$_SERVER ['DOCUMENT_ROOT'] = dirname ( __DIR__ );
+require_once dirname ( $_SERVER ['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . "src/sql.php";
+
+class DeleteAlbumImageTest extends TestCase {
+    private $http;
+    private $sql;
+
+    public function setUp() {
+        $this->http = new Client(['base_uri' => 'http://localhost:90/']);
+        $this->sql = new Sql();
+        $this->sql->executeStatement( "INSERT INTO `albums` (`id`, `name`, `description`, `location`, `owner`) VALUES ('999', 'sample-album', 'sample album for testing', 'sample', 4);" );
+        $this->sql->executeStatement( "INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (NULL, '999', '', '0', '', '/albums/sample/sample1.jpg', '300', '400', '1');" );
+        $this->sql->executeStatement( "INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (NULL, '999', '', '1', '', '/albums/sample/sample2.jpg', '300', '400', '1');" );
+        $this->sql->executeStatement( "INSERT INTO `download_rights` (`user`, `album`, `image`) VALUES ('*', '999', '1');" );
+        $oldmask = umask(0);
+        mkdir( 'content/albums/sample' );
+        chmod( 'content/albums/sample', 0777 );
+        mkdir( 'content/albums/sample/full' );
+        chmod( 'content/albums/sample/full', 0777 );
+        touch( 'content/albums/sample/sample1.jpg' );
+        chmod( 'content/albums/sample/sample1.jpg', 0777 );
+        touch( 'content/albums/sample/full/sample1.jpg' );
+        chmod( 'content/albums/sample/full/sample1.jpg', 0777 );
+        touch( 'content/albums/sample/sample2.jpg' );
+        chmod( 'content/albums/sample/sample2.jpg', 0777 );
+        touch( 'content/albums/sample/full/sample2.jpg' );
+        chmod( 'content/albums/sample/full/sample2.jpg', 0777 );
+        umask($oldmask);
+    }
+
+    public function tearDown() {
+        $this->http = NULL;
+        $this->sql->executeStatement( "DELETE FROM `albums` WHERE `albums`.`id` = 999;" );
+        $this->sql->executeStatement( "DELETE FROM `album_images` WHERE `album_images`.`album` = 999;" );
+        $this->sql->executeStatement( "DELETE FROM `download_rights` WHERE `download_rights`.`album` = 999;" );
+        $count = $this->sql->getRow( "SELECT MAX(`id`) AS `count` FROM `albums`;")['count'];
+        $count++;
+        $this->sql->executeStatement( "ALTER TABLE `albums` AUTO_INCREMENT = $count;" );
+        $count = $this->sql->getRow( "SELECT MAX(`id`) AS `count` FROM `album_images`;")['count'];
+        $count++;
+        $this->sql->executeStatement( "ALTER TABLE `album_images` AUTO_INCREMENT = $count;" );
+        system ( "rm -rf " . escapeshellarg ( 'content/albums/sample' ) );
+    }
+
+    public function testNotLoggedIn() {
+        $response;
+        try {
+            $response = $this->http->request('POST', 'api/delete-album-image.php');
+        } catch ( GuzzleHttp\Exception\ClientException $e ) {
+            $this->assertEquals(401, $e->getResponse()->getStatusCode());
+            $this->assertEquals("", $e->getResponse()->getBody() );
+        }
+    }
+
+    public function testLoggedInAsDownloader() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '5510b5e6fffd897c234cafe499f76146'
+                ], 'localhost');
+        $response;
+        try {
+            $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                    'cookies' => $cookieJar
+            ]);
+        } catch ( GuzzleHttp\Exception\ClientException $e ) {
+            $this->assertEquals(401, $e->getResponse()->getStatusCode());
+            $this->assertEquals("You do not have appropriate rights to perform this action", $e->getResponse()->getBody() );
+        }
+    }
+    
+    public function testNoAlbum() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id is required", $response->getBody());
+    }
+
+    public function testBlankAlbum() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                    'album' => ''
+                ],
+                'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id can not be blank", (string)$response->getBody());
+    }
+
+    public function testLetterAlbum() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                    'album' => 'a'
+                ],
+                'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id does not match any albums", $response->getBody());
+    }
+
+    public function testBadAlbumId() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                    'album' => 9999
+                ],
+                'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id does not match any albums", $response->getBody());
+    }
+    
+    public function testNoImage() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                    'album' => 999
+                ],
+                'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id is required", $response->getBody() );
+    }
+
+    public function testBlankImage() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                   'album' => 999,
+                   'image' => ''
+               ],
+               'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id can not be blank", (string)$response->getBody() );
+    }
+
+    public function testDeleteImage1() {
+        $cookieJar = CookieJar::fromArray([
+                    'hash' => '1d7505e7f434a7713e84ba399e937191'
+                ], 'localhost');
+        $response = $this->http->request('POST', 'api/delete-album-image.php', [
+                'form_params' => [
+                   'album' => 999,
+                   'image' => '0'
+               ],
+               'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("", $response->getBody() );
+        $images = $this->sql->getRows( "SELECT * FROM `album_images` WHERE `album_images`.`album` = 999;" );
+        $this->assertEquals( 1, sizeOf( $images ) );
+        $this->assertEquals( 2, $images[0]['id'] );
+        $this->assertEquals( 999, $images[0]['album'] );
+        $this->assertEquals( '', $images[0]['title'] );
+        $this->assertEquals( 0, $images[0]['sequence'] );
+        $this->assertEquals( '', $images[0]['caption'] );
+        $this->assertEquals( '/albums/sample/sample2.jpg', $images[0]['location'] );
+        $this->assertEquals( 300, $images[0]['width'] );
+        $this->assertEquals( 400, $images[0]['height'] );
+        $this->assertEquals( 1, $images[0]['active'] );
+        $downloadRights = $this->sql->getRow( "SELECT * FROM `download_rights` WHERE `download_rights`.`album` = 999;" );
+        $this->assertEquals( '*', $downloadRights['user'] );
+        $this->assertEquals( 999, $downloadRights['album'] );
+        $this->assertEquals( 0, $downloadRights['image'] );
+        $this->assertFalse( file_exists( 'content/albums/sample/sample1.jpg' ) );
+        $this->assertFalse( file_exists( 'content/albums/sample/full/sample1.jpg' ) );
+    }
+}
+?>
