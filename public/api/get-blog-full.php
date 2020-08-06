7447d2c9ac54552e4d9a1d040669af0f15ab9f9b
Max Saperstone <msaperst@gmail.com>
2020-08-06 11:38:45 -0400

    Adding full blog api/tests

--- public/api/get-blog-full.php
+++ public/api/get-blog-full.php
@@ -10,62 +10,34 @@ $api = new Api ($sql, $user);
 $response = array ();
 $post = 0;
 
-if (isset ( $_GET ['post'] ) && $_GET ['post'] != "") {
-    $post = ( int ) $_GET ['post'];
-} else {
-    if (! isset ( $_GET ['post'] )) {
-        echo "Post id is required!";
-    } elseif ($_GET ['post'] == "") {
-        echo "Post id cannot be blank!";
-    } else {
-        echo "Some other Post id error occurred!";
-    }
-    $conn->disconnect ();
+$post = $api->retrieveGetInt('post', 'Blog id');
+if( is_array( $post ) ) {
+    echo $post['error'];
+    exit();
+}
+$post_info = $sql->getRow( "SELECT * FROM `blog_details` WHERE id = $post;" );
+if (! $post_info ['id']) {
+    echo "Blog id does not match any blogs";
+    $sql->disconnect ();
     exit ();
 }
 
-$sql = "SELECT * FROM `blog_details` WHERE id = $post;";
-$r = $sql->getRow( $sql );
-if (! $r ['title']) {
-    echo "Blog doesn't exist!";
-    $conn->disconnect();
-    exit ();
-}
+$post_info ['date'] = date ( 'F jS, Y', strtotime ( $post_info ['date'] ) );
 
-$r ['date'] = date ( 'F jS, Y', strtotime ( $r ['date'] ) );
-$sql = "SELECT * FROM `blog_images` WHERE blog = " . $r ['id'] . ";";
-$images = mysqli_query ( $conn->db, $sql );
-$contents = array ();
-while ( $s = mysqli_fetch_assoc ( $images ) ) {
-    $contents [] = $s;
-}
-
-$sql = "SELECT * FROM `blog_texts` WHERE blog = " . $r ['id'] . ";";
-$texts = mysqli_query ( $conn->db, $sql );
-while ( $s = mysqli_fetch_assoc ( $texts ) ) {
-    $contents [] = $s;
-}
-
+$contents = $sql->getRows( "SELECT * FROM `blog_images` WHERE blog = " . $post_info ['id'] . ";" );
+$contents = array_merge( $contents, $sql->getRows( "SELECT * FROM `blog_texts` WHERE blog = " . $post_info ['id'] . ";" ) );
 foreach ( $contents as $content ) {
-    $r ['content'] [$content ['contentGroup']] [] = $content;
+    $post_info ['content'] [$content ['contentGroup']] [] = $content;
 }
 
-$sql = "SELECT `tags`.* FROM `tags` JOIN `blog_tags` ON tags.id = blog_tags.tag WHERE blog_tags.blog = " . $r ['id'] . ";";
-$tags = mysqli_query ( $conn->db, $sql );
-while ( $s = mysqli_fetch_assoc ( $tags ) ) {
-    $r ['tags'] [] = $s;
-}
-
-$sql = "SELECT * FROM `blog_comments` WHERE blog = " . $r ['id'] . " ORDER BY date desc;";
-$comments = mysqli_query ( $conn->db, $sql );
-while ( $s = mysqli_fetch_assoc ( $comments ) ) {
-    if (($s ['user'] != "" && $s ['user'] == $user->getId ()) || $user->isAdmin ()) {
-        $s ['delete'] = true;
+$post_info ['tags'] = $sql->getRows( "SELECT `tags`.* FROM `tags` JOIN `blog_tags` ON tags.id = blog_tags.tag WHERE blog_tags.blog = " . $post_info ['id'] . ";" );
+$post_info ['comments'] = $sql->getRows( "SELECT * FROM `blog_comments` WHERE blog = " . $post_info ['id'] . " ORDER BY date desc;" );
+foreach( $post_info ['comments'] as $key => $comment ) {
+    if (($comment ['user'] != "" && $comment ['user'] == $user->getId ()) || $user->isAdmin ()) {
+        $post_info ['comments'][$key]['delete'] = true;
     }
-    $r ['comments'] [] = $s;
 }
 
-echo json_encode ( $r );
-
-$conn->disconnect ();
+echo json_encode ( $post_info );
+$sql->disconnect ();
 exit ();
