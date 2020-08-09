35d1a1fae8bdc7505b779266e85a73b11aa11929
Max Saperstone <msaperstone@testpros.com>
2020-08-08 23:40:48 -0400

    Cleaning up how we're using Browser.php

--- public/api/send-error.php
+++ public/api/send-error.php
@@ -46,7 +46,6 @@ require_once "Mail/mime.php";
 
 $IP = $session->getClientIP();
 $geo_info = json_decode(file_get_contents("http://ipinfo.io/$IP/json"));
-require_once($path = dirname($_SERVER ['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . "resources/Browser.php-master/src/Browser.php");
 $browser = new Browser ();
 
 // create email body and send it
