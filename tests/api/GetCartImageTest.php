37ab22954d13d07b38f480cda1cd421766958956
Max Saperstone <msaperstone@testpros.com>
2020-08-10 13:55:42 -0400

    More tests

---
+++ tests/api/GetCartImageTest.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace api;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Exception\ClientException;
+use PHPUnit\Framework\TestCase;
+use Sql;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
+
+class GetCartImageTest extends TestCase {
+    private $http;
+    private $sql;
+
+    public function setUp() {
+        $this->http = new Client(['base_uri' => 'http://' . getenv('DB_HOST') . ':90/']);
+        $this->sql = new Sql();
+        $this->sql->executeStatement("INSERT INTO `albums` (`id`, `name`, `description`, `location`, `owner`) VALUES ('997', 'sample-album', 'sample album for testing', 'sample', 5);");
+        $this->sql->executeStatement("INSERT INTO `albums` (`id`, `name`, `description`, `location`, `owner`) VALUES ('998', 'sample-album', 'sample album for testing', 'sample', 5);");
+        $this->sql->executeStatement("INSERT INTO `albums` (`id`, `name`, `description`, `location`, `owner`) VALUES ('999', 'sample-album', 'sample album for testing', 'sample', 5);");
+        $this->sql->executeStatement('INSERT INTO `cart` VALUES(1,997,1,1,2)');
+        $this->sql->executeStatement('INSERT INTO `cart` VALUES(1,997,2,1,0)');
+        $this->sql->executeStatement('INSERT INTO `cart` VALUES(2,997,3,5,3)');
+        $this->sql->executeStatement('INSERT INTO `cart` VALUES(1,998,0,31,1)');
+        $this->sql->executeStatement('INSERT INTO `cart` VALUES(1,999,5,23,1)');
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (995, '997', 'm', '1', 'v', 'a', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (996, '997', 'n', '2', 'w', 'b', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (997, '997', 'o', '3', 'x', 'c', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (998, '998', 'p', '0', 'y', 'd', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (999, '999', 'q', '5', 'z', 'e', '300', '400', '1');");
+    }
+
+    public function tearDown() {
+        $this->http = NULL;
+        $this->sql->executeStatement("DELETE FROM `albums` WHERE `albums`.`id` = 997;");
+        $this->sql->executeStatement("DELETE FROM `albums` WHERE `albums`.`id` = 998;");
+        $this->sql->executeStatement("DELETE FROM `albums` WHERE `albums`.`id` = 999;");
+        $this->sql->executeStatement("DELETE FROM `cart` WHERE `cart`.`album` = 997;");
+        $this->sql->executeStatement("DELETE FROM `cart` WHERE `cart`.`album` = 998;");
+        $this->sql->executeStatement("DELETE FROM `cart` WHERE `cart`.`album` = 999;");
+        $this->sql->executeStatement("DELETE FROM `cart` WHERE `cart`.`user` = 1;");
+        $this->sql->executeStatement("DELETE FROM `album_images` WHERE `album_images`.`album` = 997;");
+        $this->sql->executeStatement("DELETE FROM `album_images` WHERE `album_images`.`album` = 998;");
+        $this->sql->executeStatement("DELETE FROM `album_images` WHERE `album_images`.`album` = 999;");
+        $count = $this->sql->getRow("SELECT MAX(`id`) AS `count` FROM `album_images`;")['count'];
+        $count++;
+        $this->sql->executeStatement("ALTER TABLE `album_images` AUTO_INCREMENT = $count;");
+        $this->sql->disconnect();
+    }
+
+    public function testNotLoggedIn() {
+        try {
+            $this->http->request('POST', 'api/get-cart-image.php');
+        } catch (ClientException $e) {
+            $this->assertEquals(401, $e->getResponse()->getStatusCode());
+            $this->assertEquals("You must be logged in to perform this action", (string)$e->getResponse()->getBody());
+        }
+    }
+
+    public function testNoAlbumId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id is required", (string)$response->getBody());
+    }
+
+    public function testBlankAlbumId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => ''
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id can not be blank", (string)$response->getBody());
+    }
+
+    public function testLetterAlbumId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 'a'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id does not match any albums", (string)$response->getBody());
+    }
+
+    public function testBadAlbumId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 9999
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id does not match any albums", (string)$response->getBody());
+    }
+
+    public function testNoImageId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 999
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id is required", (string)$response->getBody());
+    }
+
+    public function testBlankImageId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 999,
+                'image' => ''
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id can not be blank", (string)$response->getBody());
+    }
+
+    public function testLetterImageId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => '999',
+                'image' => 'a'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id does not match any images", (string)$response->getBody());
+    }
+
+    public function testBadImageId() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 999,
+                'image' => 9999
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id does not match any images", (string)$response->getBody());
+    }
+
+    public function testGetCartNothing() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 997,
+                'image' => 3
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(array(), json_decode($response->getBody(), true));
+    }
+
+    public function testGetCartSimple() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 997,
+                'image' => 1
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $cartDetails = json_decode($response->getBody(), true);
+        $this->assertEquals(1, sizeOf($cartDetails));
+        $this->assertEquals(1, $cartDetails[0]['user']);
+        $this->assertEquals(997, $cartDetails[0]['album']);
+        $this->assertEquals(1, $cartDetails[0]['image']);
+        $this->assertEquals(1, $cartDetails[0]['product']);
+        $this->assertEquals(2, $cartDetails[0]['count']);
+    }
+
+    public function testGetCartComplex() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/get-cart-image.php', [
+            'query' => [
+                'album' => 999,
+                'image' => 5
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $cartDetails = json_decode($response->getBody(), true);
+        $this->assertEquals(1, sizeOf($cartDetails));
+        $this->assertEquals(1, $cartDetails[0]['user']);
+        $this->assertEquals(999, $cartDetails[0]['album']);
+        $this->assertEquals(5, $cartDetails[0]['image']);
+        $this->assertEquals(23, $cartDetails[0]['product']);
+        $this->assertEquals(1, $cartDetails[0]['count']);
+    }
+}
