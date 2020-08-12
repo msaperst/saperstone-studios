54c6730d5d5b5f19cf4a84ad44c17ea3e9fcfdf9
Max Saperstone <msaperstone@testpros.com>
2020-08-12 10:48:11 -0400

    Making Chrome run headless

--- tests/ui/SampleSeleniumTest.php
+++ tests/ui/SampleSeleniumTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace ui;
 
+use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
@@ -11,7 +12,11 @@ class SampleSeleniumTest extends TestCase {
 
     public function setUp() {
         $host = 'http://127.0.0.1:4444';
-        $this->driver = RemoteWebDriver::create($host, DesiredCapabilities::firefox());
+        $chromeOptions = new ChromeOptions();
+        $chromeOptions->addArguments(['--no-sandbox', '--headless']);
+        $desiredCapabilities = DesiredCapabilities::chrome();
+        $desiredCapabilities->setCapability(ChromeOptions::CAPABILITY, $chromeOptions);
+        $this->driver = RemoteWebDriver::create($host, $desiredCapabilities);
         $this->driver->get('http://' . getenv('DB_HOST') . ':90/');
     }
 
