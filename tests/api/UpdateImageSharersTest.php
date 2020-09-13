8687c7b5b89ccdcdd7b7d67aec58e4959a6e91c5
Max Saperstone <msaperstone@testpros.com>
2020-09-13 18:24:24 -0400

    Updating image sharers

---
+++ tests/api/UpdateImageSharersTest.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace api;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\Exception\ClientException;
+use PHPUnit\Framework\TestCase;
+use Sql;
+
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
+
+class UpdateImageSharersTest extends TestCase {
+    private $http;
+    private $sql;
+
+    public function setUp() {
+        $this->http = new Client(['base_uri' => 'http://' . getenv('DB_HOST') . ':90/']);
+        $this->sql = new Sql();
+        $this->sql->executeStatement("INSERT INTO `albums` (`id`, `name`, `description`, `location`, `owner`, `code`) VALUES ('999', 'sample-album', 'sample album for testing', 'sample', 4, 123);");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (998, '999', '', '2', '', '', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `album_images` (`id`, `album`, `title`, `sequence`, `caption`, `location`, `width`, `height`, `active`) VALUES (999, '999', '', '3', '', '', '300', '400', '1');");
+        $this->sql->executeStatement("INSERT INTO `share_rights` (`user`, `album`, `image`) VALUES ('3', 999, '999');");
+    }
+
+    public function tearDown() {
+        $this->http = NULL;
+        $this->sql->executeStatement("DELETE FROM `albums` WHERE `albums`.`id` = 999;");
+        $this->sql->executeStatement("DELETE FROM `album_images` WHERE `album_images`.`album` = 999;");
+        $this->sql->executeStatement("DELETE FROM `share_rights` WHERE `album` = '999'");
+        $count = $this->sql->getRow("SELECT MAX(`id`) AS `count` FROM `albums`;")['count'];
+        $count++;
+        $this->sql->executeStatement("ALTER TABLE `albums` AUTO_INCREMENT = $count;");
+        $count = $this->sql->getRow("SELECT MAX(`id`) AS `count` FROM `album_images`;")['count'];
+        $count++;
+        $this->sql->executeStatement("ALTER TABLE `album_images` AUTO_INCREMENT = $count;");
+        $count = $this->sql->getRow("SELECT MAX(`id`) AS `count` FROM `products`;")['count'];
+        $count++;
+        $this->sql->executeStatement("ALTER TABLE `products` AUTO_INCREMENT = $count;");
+        $this->sql->disconnect();
+    }
+
+    public function testNotLoggedIn() {
+        try {
+            $this->http->request('POST', 'api/update-image-sharers.php');
+        } catch (ClientException $e) {
+            $this->assertEquals(401, $e->getResponse()->getStatusCode());
+            $this->assertEquals('', $e->getResponse()->getBody());
+        }
+    }
+
+    public function testLoggedInAsDownloader() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '5510b5e6fffd897c234cafe499f76146'
+        ], getenv('DB_HOST'));
+        try {
+            $this->http->request('POST', 'api/update-image-sharers.php', [
+                'cookies' => $cookieJar
+            ]);
+        } catch (ClientException $e) {
+            $this->assertEquals(401, $e->getResponse()->getStatusCode());
+            $this->assertEquals("You do not have appropriate rights to perform this action", $e->getResponse()->getBody());
+        }
+    }
+
+    public function testNoAlbum() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id is required", (string)$response->getBody());
+    }
+
+    public function testBlankAlbum() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => ''
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id can not be blank", (string)$response->getBody());
+    }
+
+    public function testBadAlbum() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '998'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Album id does not match any albums", (string)$response->getBody());
+    }
+
+    public function testNoImage() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '999'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id is required", (string)$response->getBody());
+    }
+
+    public function testBlankImage() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '999',
+                'image' => ''
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id can not be blank", (string)$response->getBody());
+    }
+
+    public function testBadImage() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '999',
+                'image' => '998'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("Image id does not match any images", (string)$response->getBody());
+    }
+
+    public function testNoSharers() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '999',
+                'image' => '3'
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("", (string)$response->getBody());
+        $this->assertEquals(0, $this->sql->getRowCount("SELECT * FROM share_rights WHERE album = 999 AND image=999"));
+    }
+
+    public function testSharers() {
+        $cookieJar = CookieJar::fromArray([
+            'hash' => '1d7505e7f434a7713e84ba399e937191'
+        ], getenv('DB_HOST'));
+        $response = $this->http->request('POST', 'api/update-image-sharers.php', [
+            'form_params' => [
+                'album' => '999',
+                'image' => '3',
+                'users' => [
+                    '*',
+                    '4'
+                ]
+            ],
+            'cookies' => $cookieJar
+        ]);
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals("", (string)$response->getBody());
+        $users = $this->sql->getRows("SELECT * FROM share_rights WHERE album = 999 AND image=999");
+        $this->assertEquals(2, sizeof($users));
+        $this->assertEquals('*', $users[0]['user']);
+        $this->assertEquals('999', $users[0]['album']);
+        $this->assertEquals('999', $users[0]['image']);
+        $this->assertEquals('4', $users[1]['user']);
+        $this->assertEquals('999', $users[1]['album']);
+        $this->assertEquals('999', $users[1]['image']);
+    }
+}
