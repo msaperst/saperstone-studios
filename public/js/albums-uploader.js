565c0f7cb6859ca4b7ece42654c3132ae7a326ce
Max Saperstone <msaperst@gmail.com>
2020-12-24 10:27:00 -0500

    Adding in missing functionality for uploader adding albums

--- public/js/albums-uploader.js
+++ public/js/albums-uploader.js
@@ -112,7 +112,53 @@ $(document).ready(function() {
             } ],
         });
     });
+
+    $('#add-album-div').keypress(function(e) {
+        if (e.which === 13) {
+            addAlbum();
+        }
+    });
+    $('#album-code-add').click(function() {
+        addAlbum();
+    });
 });
+
+function addAlbum() {
+    // spin our button
+    $("#album-code-add").prop("disabled", true);
+    $("#album-code-add em").removeClass('fa fa-plus-circle').addClass('glyphicon glyphicon-asterisk icon-spin');
+    // make our call
+    $.get("/api/find-album.php", {
+        code : $('#album-code').val(),
+        albumAdd : 1,
+    }).done(function(data) {
+        // goto album url if it exists
+        if ($.isNumeric(data) && data !== '0') {
+            $('#add-album-div').append("<div id='album-code-add-message' class='alert alert-info'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Added album to your list</div>");
+            // refresh our tables
+            album_table.ajax.reload(null, false);
+            setTimeout(function(){
+                $('#album-code-add-message').remove();
+            }, 10000);
+        } else if (data === '0') {
+            $('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Some unexpected error occurred while searching for your album.<br/>Please <a class='gen' target='_blank' href='mailto:admin@saperstonestudios.com'>Contact our System Administrators</a> for more details, or try resubmitting.</div>");
+        } else {
+            $('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>" + data + "</div>");
+        }
+    }).fail(function(xhr, status, error) {
+        if ( xhr.responseText !== "" ) {
+            $('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>" + xhr.responseText + "</div>");
+        } else if ( error === "Unauthorized" ) {
+            $('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Your session has timed out, and you have been logged out. Please login again, and repeat your action.</div>");
+        } else {
+            $('#add-album-div').append("<div class='alert alert-danger'><a href='#' class='close' data-dismiss='alert' aria-label='close' title='close'>×</a>Some unexpected error occurred while searching for your album.<br/>Please <a class='gen' target='_blank' href='mailto:admin@saperstonestudios.com'>Contact our System Administrators</a> for more details, or try resubmitting.</div>");
+        }
+    }).always(function(){
+        //fix our button
+        $("#album-code-add").prop("disabled", false);
+        $("#album-code-add em").addClass('fa fa-plus-circle').removeClass('glyphicon glyphicon-asterisk icon-spin');
+    });
+}
 
 function setupEdit() {
     $('.edit-album-btn').off().click(function() {
