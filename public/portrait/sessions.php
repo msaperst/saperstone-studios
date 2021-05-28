426f29733304fdbf32edd359988302ca8281f8b3
Max Saperstone <msaperst@gmail.com>
2021-05-28 10:46:02 -0400

    Develop (#70)
    * Adding in gallery fix for navigation on dots (#66)
    
    * Adding in gallery fix for navigation on dots
    
    * Adding in tests for new updates
    
    * Cleaning up commented out code
    
    * Facebook pixel (#67)
    
    * Adding in pixel code
    
    * Adding in missing files
    
    * Cleaning up tests/session; adding pipeline storage and parallelization
    
    * Initial SEO Changes (#69)
    
    * Adding in SEO fixes
    
    * Adding in new session information

--- public/portrait/sessions.php
+++ public/portrait/sessions.php
@@ -49,6 +49,10 @@ $user = User::fromSystem();
         <!-- Features Section -->
         <div class="row">
             <div class="col-lg-12">
+                <p>Saperstone Studios is a newborn and family photographer in Fairfax,
+                    VA. I love that I can offer my clients flexibility by having my own
+                    studio to offer both posed sessions as well as outdoor photography
+                    sessions.</p>
                 <p>From the moment you realize you’re having a baby, you know it’s
                     going to be an amazing journey. Capture those precious moments in
                     your families life with Saperstone Studios. The first year is when
@@ -57,7 +61,7 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">Maternity</h2>
+                <h2 class="page-header">Maternity Photography Session</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
@@ -72,11 +76,12 @@ $user = User::fromSystem();
                 </div>
             </div>
             <div class="col-xs-12" style="padding-top: 20px;">
-                <p>Be sure to document your pregnancy with a maternity session.
+                <p>Be sure to document your pregnancy with a northern virginia maternity session.
                     Every pregnancy is unique so the exact timing of your session will
                     vary. Generally the best time is between 30 and 34 weeks. We want
                     you to be showing but also comfortable. If you're expecting twins
-                    then schedule earlier!</p>
+                    then schedule earlier! Let Saperstone Studios document the start to
+                    an amazing journey and be your maternity photographer!</p>
                 <ul>
                     <li>Portraits of you and your significant other. Siblings welcome!</li>
                     <li>1 hour session at your home or on site location in Northern
@@ -90,7 +95,7 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">Newborn</h2>
+                <h2 class="page-header">Newborn Photography Session</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
@@ -105,7 +110,9 @@ $user = User::fromSystem();
                 </div>
             </div>
             <div class="col-xs-12" style="padding-top: 20px;">
-                <p>Newborns are best photographed during the first 10 days of life
+                <p>Saperstone Studios has over 10 years of experience being a Fairfax,
+                    VA newborn photographer and I'd love to share that expertise with
+                    you! Newborns are best photographed during the first 10 days of life
                     to capture the sleepy stage. Be sure to schedule these sessions
                     well in advance as I only book a limited amount of newborns per
                     week to allow for flexibility with your due date. You'll have full
@@ -122,7 +129,7 @@ $user = User::fromSystem();
                 </p>
                 <ul>
                     <li>Portraits of you and your newborn</li>
-                    <li>2-3 hour session</li>
+                    <li>2-3 hour newborn photography session</li>
                     <li>50+ fully edited images for viewing at your review session</li>
                 </ul>
                 <p>
@@ -131,7 +138,7 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">Sitting up - 6 to 8 Months</h2>
+                <h2 class="page-header">Sitter Photography Session - 6 to 8 months</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
@@ -146,9 +153,10 @@ $user = User::fromSystem();
                 </div>
             </div>
             <div class="col-xs-12" style="padding-top: 20px;">
-                <p>These sessions take place at 6-8 months, when your baby is able
-                    to sit up unassisted. At this stage your baby will be smiling,
-                    grabbing their toes, and showing off their sweet personality.</p>
+                <p>Out of all the family photography sessions I provide, this milestone
+                    is one of my favorites! These sessions take place at 6-8 months, when
+                    your baby is able to sit up unassisted. At this stage your baby will
+                    be smiling, grabbing their toes, and showing off their sweet personality.</p>
 
                 <p>Your baby can be photographed on location or at my home studio.
                     Both options are great and depend on the season/weather but also
@@ -157,7 +165,7 @@ $user = User::fromSystem();
                     backdrops and props.</p>
                 <ul>
                     <li>Studio or Outdoors (outdoors April through October only)</li>
-                    <li>45-60 minute session (2-3 setups/outfits)</li>
+                    <li>Up to 1 hour photography session (2-3 setups/outfits)</li>
                     <li>50+ fully edited images for viewing at your ordering
                         appointment</li>
                     <li>The focus of this session is on the one child, but you may
@@ -169,7 +177,7 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">Cake Smash - 1 Year</h2>
+                <h2 class="page-header">Cake Smash Photography Session</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
@@ -190,7 +198,7 @@ $user = User::fromSystem();
                     included.</p>
                 <ul>
                     <li>Studio or Outdoors (outdoors April through October only)</li>
-                    <li>45-60 minute session (2-3 setups/outfits)</li>
+                    <li>Up to 1 hour photography session (2-3 setups/outfits)</li>
                     <li>50+ fully edited images for viewing at your ordering
                         appointment</li>
                     <li>The focus of this session is on the one child, but you may
@@ -202,7 +210,67 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">Family and Kids</h2>
+                <h2 class="page-header">Milestone Photography Session Packages</h2>
+            </div>
+            <div class="col-xs-12" style="padding-top: 20px;">
+                <p>Want to capture it all? I offer two different milestone packages so you
+                    can capture your baby's photos throughout the whole first year when
+                    they grow and change the most.</p>
+            </div>
+            <div class="col-lg-12">
+                <h3 class="page-header">Bump to Baby</h3>
+            </div>
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
+                    <img src="img/bump-to-baby-1.jpg<?php echo $rand; ?>" width="100%"
+                         alt="Bump to Baby">
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
+                    <img src="img/bump-to-baby-2.jpg<?php echo $rand; ?>" width="100%"
+                         alt="Bump to Baby">
+                </div>
+            </div>
+            <div class="col-xs-12" style="padding-top: 20px;">
+                <ul>
+                    <li>Includes both a <a href="galleries.php?w=2">Maternity photography
+                        session</a> as well as a <a href="gallery.php?w=3">Newborn photography session</a></li>
+                    <li>Each photography session includes an additional complimentary $150 print credit to
+                        put towards your <a href="products.php">purchase of digitals and/or product artwork</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-12">
+                <h3 class="page-header">Watch Me Grow</h3>
+            </div>
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
+                    <img src="img/watch-me-grow-1.jpg<?php echo $rand; ?>" width="100%"
+                         alt="Watch Me Grow">
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
+                    <img src="img/watch-me-grow-2.jpg<?php echo $rand; ?>" width="100%"
+                         alt="Watch Me Grow">
+                </div>
+            </div>
+            <div class="col-xs-12" style="padding-top: 20px;">
+                <ul>
+                    <li>Includes 3 sessions total:
+                        <ol>
+                            <li><a href="galleries.php?w=35">Sitter 6 Month Photography Session</a></li>
+                            <li><a href="galleries.php?w=48">Cake Smash Photography Session</a></li>
+                            <li><a href="galleries.php?w=6">Family Photography Session</a> at some point in baby's 2nd year</li>
+                        </ol>
+                    </li>
+                    <li>Each photography session includes an additional complimentary $150 print credit
+                        to put towards your <a href="products.php">purchase of digitals and/or product artwork</a></li>
+                </ul>
+            </div>
+
+            <div class="col-lg-12">
+                <h2 class="page-header">Family and Kids Photography Session</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
@@ -222,17 +290,17 @@ $user = User::fromSystem();
                     and never smile for me so I just don't bother'. This makes me so
                     sad! Don't miss out on documenting your little ones because you
                     view the process as stressful, it doesn't have to be! Kids love to
-                    laugh. and PLAY and that's what we do at your portrait session.
-                    Every child's personality is different and I can cater to them as
-                    needed. In my experience, I've found that you can't just TELL
-                    someone to smile. It needs to come naturally and you do that by
-                    having a little fun ;) Do your kids love dinosaurs? Let's rawr! Do
-                    they love to run? Let's race!</p>
+                    laugh. and PLAY and that's what we do at your family photography
+                    portrait session. Every child's personality is different and I can
+                    cater to them as needed. In my experience, I've found that you can't
+                    just TELL someone to smile. It needs to come naturally and you do
+                    that by having a little fun ;) Do your kids love dinosaurs? Let's
+                    rawr! Do they love to run? Let's race!</p>
                 <ul>
                     <li>Immediate families, up to 5 people</li>
-                    <li>Family sessions are only available on location/outdoors (not in
-                        studio)</li>
-                    <li>1 hour session</li>
+                    <li>Family photography sessions are only available on location/outdoors
+                        (not in studio)</li>
+                    <li>Up to 1 hour family photography session</li>
                     <li>50+ fully edited images for viewing at your image review
                         session</li>
                     <li>May include any combination of portraits you would like: whole
@@ -246,7 +314,7 @@ $user = User::fromSystem();
             </div>
 
             <div class="col-lg-12">
-                <h2 class="page-header">High School Senior</h2>
+                <h2 class="page-header">High School Senior Photography Portraits</h2>
             </div>
             <div class="col-xs-6">
                 <div class="<?php if ($user->isAdmin ()) { echo " editable horizontal"; } ?>">
