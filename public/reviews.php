dc3c50b4b62330d227c13d44e5a5a814376866e0
Max Saperstone <msaperst@gmail.com>
2025-11-02 08:28:56 -0700

    Changing yelp link

--- public/reviews.php
+++ public/reviews.php
@@ -164,7 +164,7 @@ require_once dirname($_SERVER ['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . "templa
                                                              alt="Yelp">
                 <div class="overlay">
                     <br/> <br/> <br/> <a class="info" target="_blank"
-                                         href="http://www.yelp.com/biz/saperstone-studios-fairfax">See
+                                         href="https://www.yelp.com/biz/saperstone-studios-chandler">See
                         More</a>
                 </div>
             </div>
