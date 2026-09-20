f0497d9ac9d74be4eedd43404331654d94adad20
Max Saperstone <msaperst@gmail.com>
2026-09-20 07:21:52 -0700

    Serve upload form dependency locally

--- public/js/jquery.uploadfile.js
+++ public/js/jquery.uploadfile.js
@@ -7,7 +7,7 @@
  */
 (function ($) {
     if ($.fn.ajaxForm === undefined) {
-        $.getScript(("https:" === document.location.protocol ? "https://" : "http://") + "malsup.github.io/jquery.form.js");
+        $.getScript("/js/jquery.form.min.js");
     }
     var feature = {};
     feature.fileapi = $("<input type='file'/>").get(0).files !== undefined;
