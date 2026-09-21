106b2e984540f686d69efba582eb39fd5bfbf538
Max Saperstone <msaperst@gmail.com>
2026-09-21 08:46:47 -0700

    Correct API test client and status expectations

--- tests/api/ErrorDisclosureTest.php
+++ tests/api/ErrorDisclosureTest.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Exception\GuzzleException;
 use PHPUnit\Framework\TestCase;
 
 class ErrorDisclosureTest extends TestCase {
-    private Client $http;
+    private ApiTestClient $http;
     private CookieJar $adminCookies;
 
     public function setUp(): void {
