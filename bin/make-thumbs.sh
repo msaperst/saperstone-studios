d3ecd3e21c68799db9e01a2df831a7a6c69794aa
Max Saperstone <msaperst@gmail.com>
2026-09-22 12:53:50 -0700

    Use safer Bash conditional syntax

--- bin/make-thumbs.sh
+++ bin/make-thumbs.sh
@@ -7,7 +7,7 @@ output="${PARENT_DIR}/public/tmp/status.txt";
 mkdir -p "$( dirname "${output}" )";
 touch "${output}";
 
-if [ "$#" -ne 3 ]; then
+if [[ "$#" -ne 3 ]]; then
     echo "Error: Appropriate album information not provided" > "$output";
     sleep 1;
     rm "$output";
@@ -18,14 +18,14 @@ markup=$2;
 album=$3;
 location="${PARENT_DIR}/public/albums/$album";
 
-if [ ! -d "$location" ]; then
+if [[ ! -d "$location" ]]; then
     echo "Error: Album doesn't exist" > "$output";
     sleep 1;
     rm "$output";
     exit;
 fi
 
-if [ ! -d "$location/full" ]; then
+if [[ ! -d "$location/full" ]]; then
     mkdir "$location/full";
     chmod 777 "$location/full";
 fi
@@ -38,17 +38,17 @@ if ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" -e "UPDATE \`$
 fi
 
 for file in "$location"/*.*; do
-    if [ -f "$file" ]; then    #if it is a file
+    if [[ -f "$file" ]]; then    #if it is a file
         filename=$(basename "$file")
         file_info=$(identify "$file");
         file_type=$(echo "$file_info" | cut -d ' ' -f 2);
         file_size=$(echo "$file_info" | cut -d ' ' -f 3);
         width=$(echo "$file_size" | cut -d 'x' -f 1);
         height=$(echo "$file_size" | cut -d 'x' -f 2);
-        if [ "$file_type" != "TXT" ] && [ ! -f "$location/full/$filename" ]; then
+        if [[ "$file_type" != "TXT" && ! -f "$location/full/$filename" ]]; then
             echo "Resizing file $filename..." > "$output";
             cp "$file" "$location/full/";
-            if [ "$file_type" != "TXT" ] && ( [ "$width" -gt "1000" ] || [ "$height" -gt "1000" ] ); then
+            if [[ "$file_type" != "TXT" ]] && (( width > 1000 || height > 1000 )); then
                 mogrify -resize 1000x1000\> "$file";
                 convert -units PixelsPerInch -density 72 "$file" "$file";
                 #update mysql
@@ -59,11 +59,11 @@ for file in "$location"/*.*; do
                 file_location="/albums/$album/${file##*/}";
                 mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS" -e "UPDATE \`$DB_NAME\`.\`album_images\` SET width='$width', height='$height' WHERE album='$id' AND location='$file_location';"
             fi
-            if [ "$markup" == "proof" ]; then
+            if [[ "$markup" == "proof" ]]; then
                 composite -dissolve 30 -tile ../img/proof.png "$file" "$file";
-            elif [ "$markup" == "watermark" ]; then
+            elif [[ "$markup" == "watermark" ]]; then
                 composite -dissolve 85 -gravity southwest -geometry 200x150+30+0 ../img/watermark.png "$file" "$file";
-            elif [ "$markup" == "none" ]; then
+            elif [[ "$markup" == "none" ]]; then
                 echo "no watermark needed";
             else
                 echo "Markup not properly provided" > "$output";
@@ -77,7 +77,7 @@ done
 
 while IFS= read -r image_location; do
     filename=$(basename "$image_location");
-    if [ ! -f "$location/$filename" ] || [ ! -f "$location/full/$filename" ]; then
+    if [[ ! -f "$location/$filename" || ! -f "$location/full/$filename" ]]; then
         echo "Error: Thumbnail generation incomplete" > "$output";
         sleep 1;
         rm "$output";
