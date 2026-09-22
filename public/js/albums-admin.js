cacc56d6b11d7ba96ef14e09bcb6741bc1649669
Max Saperstone <msaperst@gmail.com>
2026-09-22 14:24:00 -0700

    Address Sonar JavaScript recommendations

--- public/js/albums-admin.js
+++ public/js/albums-admin.js
@@ -2,7 +2,7 @@ var album_table;
 var resultsSelected = false;
 
 function thumbnailStatus(row) {
-    if (parseInt(row.images, 10) === 0) {
+    if (Number.parseInt(row.images, 10) === 0) {
         return '<span class="label label-default">N/A</span>';
     }
     if (String(row.thumbsCreated) === '1') {
