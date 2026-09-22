1ce9dd28679a828c7e69fc9960264fe12a33e64b
Max Saperstone <msaperst@gmail.com>
2026-09-22 14:49:15 -0700

    Prevent stale album management state

--- public/api/get-album.php
+++ public/api/get-album.php
@@ -1,5 +1,9 @@
 <?php
 require_once dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
+
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+
 $api = new Api ();
 
 $api->forceLoggedIn();
