7b7d3d526e9a05219d5e120e8b26bfa208f23bfa
Max Saperstone <msaperst@gmail.com>
2026-09-22 06:48:30 -0700

    Renaming site consent form (#179)

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname ( $_SERVER ['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
+require_once dirname($_SERVER ['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
 $navUser = User::fromSystem();
 ?>
 
@@ -7,8 +7,8 @@ $navUser = User::fromSystem();
 
 <!-- User Information For JS -->
 <div class="hidden">
-    <input id="my-user-id" value="<?php echo $navUser->getId(); ?>" /> <input
-        id="my-user-role" value="<?php echo $navUser->getRole(); ?>" />
+    <input id="my-user-id" value="<?php echo $navUser->getId(); ?>"/> <input
+            id="my-user-role" value="<?php echo $navUser->getRole(); ?>"/>
 </div>
 
 <!-- Footer -->
@@ -23,7 +23,8 @@ $navUser = User::fromSystem();
         </div>
         <div class="col-md-4 text-center">
             <ul class="list-inline social-buttons">
-                <?php $iconSize = ""; require_once dirname( $_SERVER['DOCUMENT_ROOT'] ) . DIRECTORY_SEPARATOR . "templates/social-list.php"; ?>
+                <?php $iconSize = "";
+                require_once dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . "templates/social-list.php"; ?>
             </ul>
         </div>
         <div class="col-md-4 text-right">
@@ -42,39 +43,39 @@ $navUser = User::fromSystem();
 
 <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"
-    integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
-    crossorigin="anonymous"></script>
+        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+        crossorigin="anonymous"></script>
 <script src="/js/jquery.form.min.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/jquery.form.min.js'); ?>"></script>
 <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
-    integrity="sha256-KM512VNnjElC30ehFwehXjx1YCHPiQkOPmqnrWtpccM="
-    crossorigin="anonymous"></script>
+        src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-KM512VNnjElC30ehFwehXjx1YCHPiQkOPmqnrWtpccM="
+        crossorigin="anonymous"></script>
 
 <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"
-    integrity="sha256-9wRM03dUw6ABCs+AU69WbK33oktrlXamEXMvxUaF+KU="
-    crossorigin="anonymous"></script>
+        src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.min.js"
+        integrity="sha256-9wRM03dUw6ABCs+AU69WbK33oktrlXamEXMvxUaF+KU="
+        crossorigin="anonymous"></script>
 
 <!-- Popper JS -->
 <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-    crossorigin="anonymous"></script>
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
 
 <!-- Bootstrap Core JavaScript -->
 <script
-    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-    integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-    crossorigin="anonymous"></script>
+        src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
 <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/js/bootstrap-dialog.min.js"
-    integrity="sha384-A1l7AkamUsoFMYzhowbdCHQanwuyYkuwH5lCZr7HrtF++vpLSuMWRoQwFaC3d3dg"
-    crossorigin="anonymous"></script>
+        src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/js/bootstrap-dialog.min.js"
+        integrity="sha384-A1l7AkamUsoFMYzhowbdCHQanwuyYkuwH5lCZr7HrtF++vpLSuMWRoQwFaC3d3dg"
+        crossorigin="anonymous"></script>
 
 
 <!-- GDPR Cookie Handling -->
 <script src="/js/cookies.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/cookies.js'); ?>"></script>
-<script src="/js/jquery.bs.gdpr.cookies.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/jquery.bs.gdpr.cookies.js'); ?>"></script>
+<script src="/js/site-consent.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/site-consent.js'); ?>"></script>
 
 <!-- Custom Core JavaScript -->
 <script src="/js/nav.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/nav.js'); ?>"></script>
@@ -91,16 +92,20 @@ $navUser = User::fromSystem();
 <!-- Google Analytics -->
 <?php
 if (Session::useAnalytics()) {
-?>
+    ?>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-L6F5KNQDY1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'G-L6F5KNQDY1');
     </script>
-<?php
+    <?php
 }
 ?>
