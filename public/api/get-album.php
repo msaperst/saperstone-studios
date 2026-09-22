4277c64bf3e520181a96814433284b2b078292ea
Max Saperstone <msaperst@gmail.com>
2026-09-22 13:13:46 -0700

    Expose album image count to thumbnail controls

--- public/api/get-album.php
+++ public/api/get-album.php
@@ -18,6 +18,7 @@ if (!$album->canUserGetData()) {
 }
 
 $albumInfo = $album->getDataBasic();
+$albumInfo['imageCount'] = (int)$album->getDataArray()['images'];
 $albumInfo['needsThumbnails'] = $album->needsThumbnails();
 $albumInfo ['date'] = substr($albumInfo ['date'], 0, 10);
 if ($albumInfo ['code'] == NULL) {
