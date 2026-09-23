8faf033f8be734478de91090d7ed7f29c823c46b
Max Saperstone <msaperst@gmail.com>
2026-09-23 12:19:25 -0700

    Cache bust shared album JavaScript

--- public/user/index.php
+++ public/user/index.php
@@ -140,7 +140,7 @@ $user->forceLogIn();
         integrity="sha384-89aj/hOsfOyfD0Ll+7f2dobA15hDyiNb8m1dJ+rJuqgrGR+PVqNU8pybx4pbF3Cc"
         crossorigin="anonymous"></script>
     <script src="/js/jquery.uploadfile.js"></script>
-    <script src="/js/albums-common.js"></script>
+    <script src="/js/albums-common.js?v=<?php echo @filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/albums-common.js'); ?>"></script>
     <?php
     if ($user->isAdmin ()) {
         ?>
