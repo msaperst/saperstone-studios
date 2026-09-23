b54c038753dca80b522ef15b6a5cc5502a599b51
Max Saperstone <msaperst@gmail.com>
2026-09-23 10:45:53 -0700

    Enforce POST on messaging and update actions

--- public/api/sign-contract.php
+++ public/api/sign-contract.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoloader.php';
+Api::requireMethod('POST');
 require_once dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 $api = new Api();
 
