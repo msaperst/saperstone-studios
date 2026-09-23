f9966257b2fc7add485129144d1e183370755422
Max Saperstone <msaperst@gmail.com>
2026-09-22 17:08:33 -0700

    Add browser script test loader

---
+++ tests/js/helpers/load-browser-script.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const vm = require('node:vm');
+
+function loadBrowserScript(relativePath, globals = {}) {
+    const projectRoot = path.resolve(__dirname, '../../..');
+    const absolutePath = path.join(projectRoot, relativePath);
+    const context = vm.createContext({
+        console,
+        ...globals
+    });
+
+    vm.runInContext(fs.readFileSync(absolutePath, 'utf8'), context, {
+        filename: relativePath
+    });
+
+    return context;
+}
+
+module.exports = {
+    loadBrowserScript
+};
