6bfaaa7d144580e27f6005536700954bfedc2575
Max Saperstone <msaperst@gmail.com>
2026-09-22 17:20:19 -0700

    Generate JavaScript JUnit results

--- bin/test-js.sh
+++ bin/test-js.sh
@@ -7,13 +7,13 @@ ROOT="$( dirname "${DIR}" )"
 
 cd "${ROOT}"
 mkdir -p reports
-rm -f reports/js-lcov.info
+rm -f reports/js-junit.xml reports/js-lcov.info
 
 node --test \
     --experimental-test-coverage \
     --test-coverage-include='public/js/*.js' \
-    --test-reporter=spec \
-    --test-reporter-destination=stdout \
+    --test-reporter=junit \
+    --test-reporter-destination=reports/js-junit.xml \
     --test-reporter=lcov \
     --test-reporter-destination=reports/js-lcov.info \
     tests/js/*.test.js
