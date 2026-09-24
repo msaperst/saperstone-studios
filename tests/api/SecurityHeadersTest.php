7b7efc053d8872e60a7a859f774b9e30d75af3ae
Max Saperstone <msaperst@gmail.com>
2026-09-24 07:15:55 -0700

    Allow legacy runtime style attributes

--- tests/api/SecurityHeadersTest.php
+++ tests/api/SecurityHeadersTest.php
@@ -87,7 +87,7 @@ class SecurityHeadersTest extends TestCase {
             $this->assertStringContainsString('frame-src https://www.google.com', $headers[0], $path);
             $this->assertSame(1, preg_match('/(?:^|;\\s*)style-src\\s+([^;]+)/', $headers[0], $styleSource), $path);
             $this->assertStringNotContainsString("'unsafe-inline'", $styleSource[1], $path);
-            $this->assertStringContainsString("style-src-attr 'none'", $headers[0], $path);
+            $this->assertStringContainsString("style-src-attr 'unsafe-inline'", $headers[0], $path);
             $this->assertMatchesRegularExpression(
                 '/img-src[^;]*https:\\/\\/cdn\\.datatables\\.net/',
                 $headers[0],
