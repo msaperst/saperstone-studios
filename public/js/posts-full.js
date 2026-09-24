95c5b99b26e87c8196d7613d3379a11d7674388f
Max Saperstone <msaperst@gmail.com>
2026-09-24 12:07:31 -0700

    Fix full blog loader pagination guard

--- public/js/posts-full.js
+++ public/js/posts-full.js
@@ -22,7 +22,7 @@ PostsFull.prototype.loadPosts = function () {
         loadPost(data, "<h2>");
 
         // when we done, see if we need to load more
-        if ($('footer').isOnScreen() && Posts.loaded < Posts.totalImages) {
+        if ($('footer').isOnScreen() && PostsFull.loaded < PostsFull.totalPosts) {
             PostsFull.loadPosts();
         }
     }, "json");
