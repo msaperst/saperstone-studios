478c4d588ccb3a98cca8403bf969502287b96539
Max Saperstone <msaperst@gmail.com>
2026-09-23 17:36:25 -0700

    Move carousel backgrounds out of inline styles

--- public/portrait/index.php
+++ public/portrait/index.php
@@ -82,7 +82,7 @@ $user = User::fromSystem();
                             }
                             echo "<div class='item$active_class'>";
                             echo "    <div class='contain'";
-                            echo "        style=\"background-image: url('" . $image ['location'] . "');\"></div>";
+                            echo "        data-background-image=\"" . htmlspecialchars($image['location'], ENT_QUOTES, 'UTF-8') . "\"></div>";
                             echo "    <div class='carousel-caption'>";
                             echo "        <h2>" . $image ['caption'] . "</h2>";
                             echo "    </div>";
