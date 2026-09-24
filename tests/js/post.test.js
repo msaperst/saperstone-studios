0a88361bbf7765dd095de641ea32691827a0a12a
Max Saperstone <msaperst@gmail.com>
2026-09-24 12:43:31 -0700

    Load shared blog helpers in post tests

--- tests/js/post.test.js
+++ tests/js/post.test.js
@@ -1,7 +1,7 @@
 const assert = require('node:assert/strict');
 const {test} = require('node:test');
 const {createJQueryEnvironment} = require('./helpers/jquery-mock');
-const {loadBrowserScript} = require('./helpers/load-browser-script');
+const {loadBrowserScripts} = require('./helpers/load-browser-script');
 
 function equalStructure(actual, expected) {
     assert.deepEqual(
@@ -57,7 +57,7 @@ function createPostContext(options = {}) {
         globals.hasCookiePreference = () => options.socialAllowed;
     }
 
-    const context = loadBrowserScript('public/js/post.js', globals);
+    const context = loadBrowserScripts(['public/js/blog-common.js', 'public/js/post.js'], globals);
     return {context, environment, scripts, windowObject};
 }
 
