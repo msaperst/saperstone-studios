40781ba6421fe88b482da9fcf3cc5a5169bcb6c3
Max Saperstone <msaperst@gmail.com>
2026-09-24 13:38:06 -0700

    Strengthen blog shared script ordering test

--- tests/js/blog-script-order.test.js
+++ tests/js/blog-script-order.test.js
@@ -6,26 +6,29 @@ const {test} = require('node:test');
 const projectRoot = path.resolve(__dirname, '../..');
 
 const pages = [
-    ['public/blog/post.php', '/js/post.js'],
-    ['public/blog/new.php', '/js/post-admin.js'],
-    ['public/blog/manage.php', '/js/post-admin.js'],
-    ['public/blog/posts.php', '/js/posts.js'],
-    ['public/blog/search.php', '/js/posts-search.js'],
-    ['public/blog/index.php', '/js/posts-full.js'],
-    ['public/blog/category.php', '/js/posts-full.js']
+    ['public/blog/post.php', ['/js/post.js', '/js/post-full.js']],
+    ['public/blog/new.php', ['/js/post-admin.js']],
+    ['public/blog/manage.php', ['/js/post-admin.js', '/js/posts-manage.js']],
+    ['public/blog/posts.php', ['/js/post.js', '/js/posts.js']],
+    ['public/blog/search.php', ['/js/post.js', '/js/posts-search.js']],
+    ['public/blog/index.php', ['/js/post.js', '/js/posts-full.js']],
+    ['public/blog/category.php', ['/js/post.js', '/js/posts-full.js']]
 ];
 
-test('blog pages load shared helpers before dependent blog scripts', () => {
-    for (const [page, dependentScript] of pages) {
+test('blog pages load shared helpers before every dependent blog script', () => {
+    for (const [page, dependentScripts] of pages) {
         const source = fs.readFileSync(path.join(projectRoot, page), 'utf8');
         const commonIndex = source.indexOf('/js/blog-common.js');
-        const dependentIndex = source.indexOf(dependentScript);
 
         assert.notEqual(commonIndex, -1, `${page} should load blog-common.js`);
-        assert.notEqual(dependentIndex, -1, `${page} should load ${dependentScript}`);
-        assert.ok(
-            commonIndex < dependentIndex,
-            `${page} should load blog-common.js before ${dependentScript}`
-        );
+
+        for (const dependentScript of dependentScripts) {
+            const dependentIndex = source.indexOf(dependentScript);
+            assert.notEqual(dependentIndex, -1, `${page} should load ${dependentScript}`);
+            assert.ok(
+                commonIndex < dependentIndex,
+                `${page} should load blog-common.js before ${dependentScript}`
+            );
+        }
     }
 });
