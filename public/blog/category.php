a6233eda9d55964e5f9f5d994d155038ef4b0673
Max Saperstone <msaperst@gmail.com>
2026-09-24 12:41:33 -0700

    Use shared blog JavaScript helpers

--- public/blog/category.php
+++ public/blog/category.php
@@ -61,7 +61,7 @@ $sql->disconnect();
 
 <!-- Gallery JavaScript -->
 <script src="/js/post.js"></script>
-<script src="/js/posts-common.js"></script>
+<script src="/js/blog-common.js"></script>
 <script src="/js/posts-full.js"></script>
 
 <!-- Script to Activate the Gallery -->
