4350a74017f752651e87f4b9503a72dc90759d5a
Max Saperstone <msaperst@gmail.com>
2026-09-24 22:58:12 -0700

    Add CSP-safe facebook-pixel.js initialization

---
+++ public/js/facebook-pixel.js
@@ -0,0 +1,19 @@
+!function (f, b, e, v, n, t, s) {
+    if (f.fbq) return;
+    n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+    };
+    if (!f._fbq) f._fbq = n;
+    n.push = n;
+    n.loaded = true;
+    n.version = '2.0';
+    n.queue = [];
+    t = b.createElement(e);
+    t.async = true;
+    t.src = v;
+    s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s);
+}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '269624791467010');
+fbq('track', 'PageView');
