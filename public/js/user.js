031b61dfc7e0e1fd31c76c59ebf38a342caa22b5
Max Saperstone <msaperst@gmail.com>
2026-09-25 12:03:52 -0700

    Handle numeric user active status in edit dialog

--- public/js/user.js
+++ public/js/user.js
@@ -209,8 +209,8 @@ function editUser(data) {
             activeInput.attr('id', 'user-active');
             activeInput.attr('type', 'checkbox');
             activeInput.prop('checked', true);
-            if (data !== null && data.hasOwnProperty('active') && data.active !== "1") {
-                activeInput.prop('checked', false);
+            if (data !== null && data.hasOwnProperty('active')) {
+                activeInput.prop('checked', Number(data.active) === 1);
             }
             inputs.append(activeDiv.append(activeInputDiv.append(activeInputDivDiv.append(activeLabel.append(activeInput).append(" Active User")))));
 
