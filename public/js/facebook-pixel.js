242b26545bc241ac4cd9a468e90d4dce004d4d1f
Max Saperstone <msaperst@gmail.com>
2026-09-25 08:56:26 -0700

    Resolve Sonar JavaScript findings

--- public/js/facebook-pixel.js
+++ public/js/facebook-pixel.js
@@ -1,18 +1,25 @@
-!function (f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+!function (windowObject, documentObject, elementName, sourceUrl) {
+    if (windowObject.fbq) return;
+
+    const pixelQueue = function (...args) {
+        if (pixelQueue.callMethod) {
+            pixelQueue.callMethod(...args);
+        } else {
+            pixelQueue.queue.push(args);
+        }
     };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = true;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = true;
-    t.src = v;
-    s = b.head || b.documentElement;
-    s.appendChild(t);
+    windowObject.fbq = pixelQueue;
+    if (!windowObject._fbq) windowObject._fbq = pixelQueue;
+    pixelQueue.push = pixelQueue;
+    pixelQueue.loaded = true;
+    pixelQueue.version = '2.0';
+    pixelQueue.queue = [];
+
+    const script = documentObject.createElement(elementName);
+    script.async = true;
+    script.src = sourceUrl;
+    const parent = documentObject.head || documentObject.documentElement;
+    parent.appendChild(script);
 }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
 window.fbq('init', '269624791467010');
