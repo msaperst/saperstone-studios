59a94de7f22a9e7c6eba24486203e61ac032941d
Max Saperstone <msaperst@gmail.com>
2026-09-24 22:41:36 -0700

    Fix carousel element identity in unit test

--- tests/js/carousel.test.js
+++ tests/js/carousel.test.js
@@ -8,13 +8,14 @@ test('carousel applies deferred background images and 3:2 sizing', () => {
     const background = environment.element('[data-background-image]');
     background.attr('data-background-image', '/img/photo.jpg');
     background.nativeElement.style = {};
-    background.each = function (callback) { callback.call(background.nativeElement); return this; };
+    background.each = function (callback) { callback.call(background); return this; };
     const carousel = environment.element('.carousel-three-by-two');
     carousel.widthValue = 600;
 
     loadBrowserScript('public/js/carousel.js', {$: environment.$, document: environment.document});
     environment.runReady();
 
+    assert.equal(background.styles.backgroundImage, undefined);
     assert.equal(background.nativeElement.style.backgroundImage, 'url("/img/photo.jpg")');
     assert.equal(carousel.heightValue, 400);
 });
