e61670bc6699ee08a6a0b002ee5494df1b84a8d0
Max Saperstone <msaperst@gmail.com>
2026-09-24 22:59:49 -0700

    Replace generated inline blog edit handler

--- public/js/posts-manage.js
+++ public/js/posts-manage.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
                 "searchable": false,
                 "data": function (row) {
                     var buttons = '<button type="button" class="btn btn-xs btn-info quick-edit-post-btn" data-toggle="tooltip" data-placement="right" title="Edit Post Details"><i class="fa fa-pencil-square-o"></i></button>';
-                    buttons += ' <button type="button" class="btn btn-xs btn-warning edit-post-btn" data-toggle="tooltip" data-placement="right" title="Edit Full Post" onclick="window.location.href=\'/blog/new.php?p=' + row.id + '\'">' + '<i class="fa fa-pencil-square-o"></i></button>';
+                    buttons += ' <button type="button" class="btn btn-xs btn-warning edit-post-btn" data-post-id="' + row.id + '" data-toggle="tooltip" data-placement="right" title="Edit Full Post">' + '<i class="fa fa-pencil-square-o"></i></button>';
                     return buttons;
                 },
                 "targets": 0
